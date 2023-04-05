aacad7306c6c203122c869cea4d90db73e0171bf
uttamapaksa <122511728+uttamapaksa@users.noreply.github.com>
2023-04-05 21:50:29 +0900

    [Gold V] Title: 뱀과 사다리 게임, Time: 152 ms, Memory: 114436 KB -BaekjoonHub

---
+++ 백준/Gold/16928. 뱀과 사다리 게임/뱀과 사다리 게임.py
@@ -0,0 +1,20 @@
+from collections import deque
+
+N, M = map(int, input().split())
+board = {i: i for i in range(1, 101)}
+visit = {i: 0 for i in range(1, 101)}
+for _ in range(N + M):
+    a, b = map(int, input().split())
+    board[a] = b
+
+q = deque([(1, 0)])
+while q:
+    s, cnt = q.popleft()
+    if visit[s]: continue
+    visit[s] = 1
+    if s == 100: print(cnt); break
+    for i in range(1, 7):
+        if s + i < 101:
+            q.append((board[s + i], cnt + 1))
+        else:
+            q.append((board[s], cnt + 1))
