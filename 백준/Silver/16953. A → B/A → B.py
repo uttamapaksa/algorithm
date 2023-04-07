6ba2b45215dfb9d7cf1a0073675b4e55580bcbb6
uttamapaksa <122511728+uttamapaksa@users.noreply.github.com>
2023-04-07 22:13:21 +0900

    [Silver II] Title: A → B, Time: 144 ms, Memory: 114436 KB -BaekjoonHub

--- 백준/Silver/16953. A → B/A → B.py
+++ 백준/Silver/16953. A → B/A → B.py
@@ -1,16 +1,23 @@
+from collections import deque
+
 A, B = map(int, input().split())
-dp = {B: 1}
-for i in range(B, A-1, -1):
-    if i not in dp: continue
-    if not i % 2:
-        n = i // 2
-        if n >= A:
-            if n in dp: dp[n] = min(dp[n], dp[i] + 1)
-            else: dp[n] = dp[i] + 1
-    elif i % 10 == 1:
-        n = i // 10
-        if n >= A:
-            if n in dp: dp[n] = min(dp[n], dp[i] + 1)
-            else: dp[n] = dp[i] + 1
-if A in dp: print(dp[A])
-else: print(-1)
+visit = {B: 1}
+q = deque([B])
+found = 0
+while q:
+    s = q.popleft()
+    if s == A:
+        print(visit[A]);
+        found = 1;
+        break
+    if not s % 2:
+        e = s // 2
+        if e >= A and e not in visit:
+            visit[e] = visit[s] + 1
+            q.append(e)
+    elif s % 10 == 1:
+        e = s // 10
+        if e >= A and e not in visit:
+            visit[e] = visit[s] + 1
+            q.append(e)
+if not found: print(-1)
