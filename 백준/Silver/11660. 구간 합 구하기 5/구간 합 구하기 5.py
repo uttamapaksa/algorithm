100de55f460dda60976fdfe9d4e9e17e7e50a155
uttamapaksa <122511728+uttamapaksa@users.noreply.github.com>
2023-04-08 06:38:21 +0900

    [Silver I] Title: 구간 합 구하기 5, Time: 448 ms, Memory: 126908 KB -BaekjoonHub

--- 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.py
+++ 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.py
@@ -1,20 +1,25 @@
 import sys
 input = sys.stdin.readline
 
+# 이전 합들을 배열에 저장
 N, M = map(int, input().split())
-arr = []
-for _ in range(N):
-    tmp = [*map(int, input().split())]
+arr = [[*map(int, input().split())] for _ in range(N)]
+for i in range(N):
+    for j in range(1, N):
+        arr[i][j] += arr[i][j-1]
+for j in range(N):
     for i in range(1, N):
-        tmp[i] += tmp[i-1]
-    arr.append(tmp)
+        arr[i][j] += arr[i-1][j]
+
+# 선택된 합 - 선택 안 된 합
 for _ in range(M):
-    x1, y1, x2, y2 = map(int, input().split())
-    x1, y1, x2, y2, ans = x1 - 1, y1 - 1, x2 - 1, y2 - 1, 0
-    if not y1:
-        for i in range(x1, x2 + 1):
-            ans += arr[i][y2]
+    r1, c1, r2, c2 = map(int, input().split())
+    r1, c1, r2, c2, ans = r1 - 1, c1 - 1, r2 - 1, c2 - 1, 0
+    if not r1 and not c1:
+        print(arr[r2][c2])
+    elif not r1:
+        print(arr[r2][c2] - arr[r2][c1-1])
+    elif not c1:
+        print(arr[r2][c2] - arr[r1-1][c2])
     else:
-        for i in range(x1, x2 + 1):
-            ans += arr[i][y2] - arr[i][y1 - 1]
-    print(ans)
+        print(arr[r2][c2] - arr[r1-1][c2] - arr[r2][c1-1] + arr[r1-1][c1-1])
