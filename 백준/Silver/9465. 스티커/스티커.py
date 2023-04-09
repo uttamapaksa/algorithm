e486154b75c77f0be5c396a8d761b38651f30ce0
uttamapaksa <122511728+uttamapaksa@users.noreply.github.com>
2023-04-09 11:24:16 +0900

    [Silver I] Title: 스티커, Time: 440 ms, Memory: 241432 KB -BaekjoonHub

--- 백준/Silver/9465. 스티커/스티커.py
+++ 백준/Silver/9465. 스티커/스티커.py
@@ -1,7 +1,14 @@
+import sys
+input = sys.stdin.readline
 for _ in range(int(input())):
+    
     n = int(input())
     arr = [[*map(int, input().split())] for _ in range(2)]
-    dp = {0: [arr[0][0], arr[1][0], 0]}
+    dp = [0] * n
+    dp[0] = (arr[0][0], arr[1][0], 0)
+    
     for i in range(1, n):
-        dp[i] = [max(dp[i-1][1], dp[i-1][2]) + arr[0][i], max(dp[i-1][0], dp[i-1][2]) + arr[1][i], max(dp[i-1][0], dp[i-1][1])]
-    print(max(dp[n-1]))
+        A, B, C = dp[i-1]
+        dp[i] = (max(B, C) + arr[0][i], max(A, C) + arr[1][i], max(A, B))
+        
+    print(max(dp[-1]))
