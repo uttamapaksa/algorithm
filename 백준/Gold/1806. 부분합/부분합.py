523a6c23245532c42c28abd9a883080e8c363657
uttamapaksa <122511728+uttamapaksa@users.noreply.github.com>
2023-04-13 06:23:00 +0900

    [Gold IV] Title: 부분합, Time: 180 ms, Memory: 125184 KB -BaekjoonHub

---
+++ 백준/Gold/1806. 부분합/부분합.py
@@ -0,0 +1,22 @@
+import sys; input = sys.stdin.readline
+
+N, S = map(int, input().split())
+arr = [*map(int, input().split())]
+
+ans = 100001
+tmp = 0
+cnt = 0
+for i in range(N):
+    cnt += 1
+    tmp += arr[i]
+    if tmp >= S:
+        ans = min(ans, cnt)
+        for j in range(cnt-1, 0, -1):
+            if tmp - arr[i-j] >= S:
+                tmp -= arr[i-j]
+                cnt -= 1
+                ans = (min(ans, cnt))
+            else:
+                break
+
+print(ans % 100001)
