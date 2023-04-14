d1298aaced96de26fa02d5e33711ad0ab214958d
uttamapaksa <122511728+uttamapaksa@users.noreply.github.com>
2023-04-15 05:48:54 +0900

    [Gold IV] Title: 스도쿠, Time: 2948 ms, Memory: 130556 KB -BaekjoonHub

--- 백준/Gold/2239. 스도쿠/스도쿠.py
+++ 백준/Gold/2239. 스도쿠/스도쿠.py
@@ -1,3 +1,6 @@
+import sys; input = sys.stdin.readline
+
+
 def next_idx(r, c):
     for j in range(c+1, 9):
         if not arr[r][j]:
@@ -14,7 +17,7 @@ def sudoku(r, c):
     R = arr_row[r]
     C = arr_col[c]
     S = arr_33[(r//3) * 3 + (c//3)]
-
+    
     for n in range(1, 10):
         if n in R or n in C or n in S: continue
         arr[r][c] = n; R.add(n); C.add(n); S.add(n)
@@ -24,29 +27,12 @@ def sudoku(r, c):
         arr[r][c] = 0; R.remove(n); C.remove(n); S.remove(n)
 
 
-arr = [[*map(int, input())] for _ in range(9)]
+arr = [[*map(int, input().rstrip())] for _ in range(9)]
 arr_row = [{arr[i][j] for j in range(9)} for i in range(9)]
 arr_col = [{arr[i][j] for i in range(9)} for j in range(9)]
 arr_33 = [{arr[i][j], arr[i][j+1], arr[i][j+2], arr[i+1][j], arr[i+1][j+1], arr[i+1][j+2], arr[i+2][j], arr[i+2][j+1], arr[i+2][j+2]} for i in range(0, 9, 3) for j in range(0, 9, 3)]
 
-for row in arr_row:
-    if 0 in row:
-        row.remove(0)
-for col in arr_col:
-    if 0 in col:
-        col.remove(0)
-for square in arr_33:
-    if 0 in square:
-        square.remove(0)
-
-for i in range(9):
-    found = 0
-    for j in range(9):
-        if not arr[i][j]:
-            found = 1
-            sudoku(i, j)
-            break
-    if found: break
+sudoku(*next_idx(0, -1))
 
 for i in arr:
     for j in i:
