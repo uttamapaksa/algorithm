e23516b7d6f7e91d4dac437e64f89d2669a9eac5
uttamapaksa <122511728+uttamapaksa@users.noreply.github.com>
2023-04-18 13:07:59 +0900

    [Gold III] Title: 소수의 연속합, Time: 1144 ms, Memory: 340632 KB -BaekjoonHub

--- 백준/Gold/1644. 소수의 연속합/소수의 연속합.py
+++ 백준/Gold/1644. 소수의 연속합/소수의 연속합.py
@@ -1,11 +1,10 @@
 N = int(input())
 prime = set(range(2, N+1))
 
-for i in range(2, N+1):
-    for j in range(2, int(i ** 0.5) + 1):
-        if not i % j:
-            prime.discard(i)
-            break
+for i in range(2, int(N ** 0.5) + 1):
+    if i in prime:
+        for j in range(i * 2, N+1, i):
+            prime.discard(j)
 
 l = len(prime)
 prime = list(prime)
