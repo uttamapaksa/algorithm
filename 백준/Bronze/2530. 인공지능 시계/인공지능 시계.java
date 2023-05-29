b4b5291d3cc08609aceb699784bffe3276af1eeb
uttamapaksa <122511728+uttamapaksa@users.noreply.github.com>
2023-05-29 17:38:15 +0900

    [Bronze IV] Title: 인공지능 시계, Time: 76 ms, Memory: 11536 KB -BaekjoonHub

---
+++ 백준/Bronze/2530. 인공지능 시계/인공지능 시계.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String [] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int h = Integer.parseInt(st.nextToken());
+		int m = Integer.parseInt(st.nextToken());
+		int s = Integer.parseInt(st.nextToken());
+		int t = Integer.parseInt(br.readLine());
+		s += t;
+		m += s / 60;
+		h += m / 60;
+		s %= 60;
+		m %= 60;
+		h %= 24;
+		System.out.println(h + " " + m + " " + s);
+	}
+}
