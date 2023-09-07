a703e888c9a09bc1d5e25fe04c982914b382607a
uttamapaksa <122511728+uttamapaksa@users.noreply.github.com>
2023-09-07 23:50:36 +0900

    [Gold IV] Title: 인구 이동, Time: 968 ms, Memory: 124120 KB -BaekjoonHub

--- 백준/Gold/16234. 인구 이동/인구 이동.py
+++ 백준/Gold/16234. 인구 이동/인구 이동.py
@@ -16,25 +16,29 @@ while moved:
 
     for x in range(N):
         for y in range(N):
-            if (x, y) in V: continue
+            if (100 * x + y) in V: continue
             
             Q.append((x, y))
-            V.add((x, y))
-            M = {(x, y)}
+            V.add(100 * x + y)
+            M = [(x, y)]
+            sums = arr[x][y]
+            sumlen = 1
 
             while Q:
                 r, c = Q.popleft()
                 for i in range(4):
                     nr, nc = r + dr[i], c + dc[i]
-                    if nr < 0 or nr >= N or nc < 0 or nc >= N or (nr, nc) in V: continue
+                    if nr < 0 or nr >= N or nc < 0 or nc >= N or (100 * nr + nc) in V: continue
                     if L <= abs(arr[r][c] - arr[nr][nc]) <= R:
                         Q.append((nr, nc))
-                        V.add((nr, nc))
-                        M.add((nr, nc))
-            
-            if len(M) > 1:
+                        V.add(100 * nr + nc)
+                        M.append((nr, nc))
+                        sums += arr[nr][nc]
+                        sumlen += 1
+                        
+            if sumlen > 1:
                 moved = True
-                avg = sum(arr[r][c] for r, c in M) // len(M)
+                avg = sums // sumlen
                 for r, c in M:
                     arr[r][c] = avg
 
