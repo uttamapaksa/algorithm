06419940d70b0f0e24f7ae17777a81bb331c83a2
uttamapaksa <122511728+uttamapaksa@users.noreply.github.com>
2023-09-10 12:56:22 +0900

    [Gold IV] Title: 공유기 설치, Time: 536 ms, Memory: 38984 KB -BaekjoonHub

--- 백준/Gold/2110. 공유기 설치/공유기 설치.py
+++ 백준/Gold/2110. 공유기 설치/공유기 설치.py
@@ -4,16 +4,10 @@ N, C = map(int, input().split())
 arr = [int(input()) for _ in range(N)]
 arr.sort()
 
-first = arr[0]  # 시작점
-minv = arr[1] - first
-pre = arr[1]
-for i in range(2, N):
-    cur = arr[i]
-    minv = min(minv, cur - pre)
-    pre = cur
-start = minv  # 최소간격
-end = arr[N-1] - first  # 최대간격
-ans = 1  # 정답간격
+first = arr[0]
+start = 1
+end = arr[N-1] - first
+ans = 1
 
 while start <= end:
     mid = (start + end) // 2
@@ -24,7 +18,7 @@ while start <= end:
         if arr[i] >= now + mid:
             now = arr[i]
             cnt += 1
-            if cnt > C: break
+            if cnt >= C: break
     
     if cnt >= C:
         start = mid + 1   
