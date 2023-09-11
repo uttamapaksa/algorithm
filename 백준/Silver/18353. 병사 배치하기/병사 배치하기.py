73a7dded8e04c11c86cb37b87ad7f5bde3e9379f
uttamapaksa <122511728+uttamapaksa@users.noreply.github.com>
2023-09-12 00:11:15 +0900

    [Silver II] Title: 병사 배치하기, Time: 780 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/18353. 병사 배치하기/병사 배치하기.py
@@ -0,0 +1,10 @@
+N = int(input())
+arr = [*map(int, input().split())]
+
+dp = [1] * N
+for i in range(1, N):
+    for j in range(i):
+        if arr[i] < arr[j]:
+            dp[i] = max(dp[i], dp[j] + 1)
+
+print(N - max(dp))
