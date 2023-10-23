26d7de13d18ae4307312d985ab29db09cf62f7d4
uttamapaksa <122511728+uttamapaksa@users.noreply.github.com>
2023-10-24 04:38:15 +0900

    [Gold IV] Title: 도시 분할 계획, Time: 2384 ms, Memory: 271044 KB -BaekjoonHub

--- 백준/Gold/1647. 도시 분할 계획/도시 분할 계획.py
+++ 백준/Gold/1647. 도시 분할 계획/도시 분할 계획.py
@@ -1,33 +1,28 @@
-import sys; input = sys.stdin.readline
+from collections import deque
+
+N, M = map(int, input().split())
+P = [i for i in range(N+1)]
+G = []
+
+for _ in range(M):
+    G.append(tuple(map(int, input().split())))
+G.sort(key=lambda x: x[2])
 
 def find(x):
     if P[x] != x:
         P[x] = find(P[x])
     return P[x]
 
-V, E = map(int, input().split())
-P = [i for i in range(V+1)]
-tmp = [{} for _ in range(V+1)]
-G = []
+# sums = maxs = cnt = 0
 MST = []
-
-for _ in range(E):
-    s, e, w = map(int, input().split())
-    if s > e: s, e = e, s
-    if e not in tmp[s]: tmp[s][e] = w
-    else: tmp[s][e] = min(tmp[s][e], w)
-
-for s in range(len(tmp)):
-    for e, w in tmp[s].items():
-        G.append((w, s, e))
-G.sort()
-
-sums = 0
-for w, s, e in G:
-    a, b = find(s), find(e)
-    if a != b:
-        P[b] = a
-        sums += w
+for u, v, w in G:
+    u, v = find(u), find(v)
+    if u != v:
+        if u < v: P[v] = u
+        else: P[u] = v
         MST.append(w)
+        # cnt += 1
+        # sums += w
+        # maxs = max(maxs, w)
 
-print(sums - max(MST))
+print(sum(MST) - max(MST))
