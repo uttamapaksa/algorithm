1573d84a80a6e25a04653ce0d010b4499a9686ad
uttamapaksa <122511728+uttamapaksa@users.noreply.github.com>
2023-11-26 15:55:59 +0900

    [Silver I] Title: 가장 가까운 세 사람의 심리적 거리, Time: 196 ms, Memory: 38676 KB -BaekjoonHub

---
+++ 백준/Silver/20529. 가장 가까운 세 사람의 심리적 거리/가장 가까운 세 사람의 심리적 거리.py
@@ -0,0 +1,17 @@
+def solution():
+    n = int(input())
+    arr = input().split()
+
+    if n >= 33:
+        return 0
+    minv = 8
+    for i in range(n-2):
+        A = arr[i]
+        for j in range(i+1, n-1):
+            B = arr[j]
+            for k in range(j+1, n):
+                minv = min(minv, (len(set(A + B + arr[k])) - 4) * 2)
+    return minv
+
+for _ in range(int(input())):
+      print(solution())
