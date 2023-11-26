af24c89bb312f96e46409d436d2b316c418de45f
uttamapaksa <122511728+uttamapaksa@users.noreply.github.com>
2023-11-26 20:54:54 +0900

    [Silver II] Title: 헌내기는 친구가 필요해, Time: 224 ms, Memory: 119456 KB -BaekjoonHub

--- 백준/Silver/21736. 헌내기는 친구가 필요해/헌내기는 친구가 필요해.py
+++ 백준/Silver/21736. 헌내기는 친구가 필요해/헌내기는 친구가 필요해.py
@@ -1,33 +1,24 @@
 from collections import deque
 
-def sol():
-    n, m = map(int, input().split())
-    A = [input() for _ in range(n)]
-    V = [[0] * m for _ in range(n)]
+n, m = map(int, input().split())
+A = [input() for _ in range(n)]
+V = [[0] * m for _ in range(n)]
 
-    def doyeon_idx():
-        for i in range(n):
-            for j in range(m):
-                if A[i][j] == 'I':
-                    return (i, j)
-                
-    i, j = doyeon_idx()
-    Q = deque([(i, j)])
-    V[i][j] = 1
+for i in range(n):
+    for j in range(m):
+        if A[i][j] == 'I':
+            Q = deque([(i, j)])
+            V[i][j] = 1
 
-    def bfs():
-        ans = 0
-        while Q:
-            r, c = Q.popleft()
-            for (dr, dc) in [(-1, 0), (0, -1), (1, 0), (0, 1)]:
-                nr, nc = r + dr, c + dc
-                if nr < 0 or nr >= n or nc < 0 or nc >= m or V[nr][nc] or A[nr][nc] == 'X': continue
-                if A[nr][nc] == 'P':
-                    ans += 1
-                Q.append((nr, nc))
-                V[nr][nc] = 1
-        return ans or 'TT'
-    
-    print(bfs())
-
-sol()
+ans = 0
+while Q:
+    r, c = Q.popleft()
+    for (dr, dc) in [(-1, 0), (0, -1), (1, 0), (0, 1)]:
+        nr, nc = r + dr, c + dc
+        if nr < 0 or nr >= n or nc < 0 or nc >= m or V[nr][nc] or A[nr][nc] == 'X': continue
+        if A[nr][nc] == 'P':
+            ans += 1
+        Q.append((nr, nc))
+        V[nr][nc] = 1
+        
+print(ans or 'TT')
