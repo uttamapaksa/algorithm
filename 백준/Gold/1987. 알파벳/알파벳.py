cf93a2c062a02cf1fb8d2e155d1671a0c1e3e7ef
uttamapaksa <122511728+uttamapaksa@users.noreply.github.com>
2023-11-27 17:33:28 +0900

    [Gold IV] Title: 알파벳, Time: 6760 ms, Memory: 125312 KB -BaekjoonHub

--- 백준/Gold/1987. 알파벳/알파벳.py
+++ 백준/Gold/1987. 알파벳/알파벳.py
@@ -3,20 +3,18 @@ dc = (0, -1, 0, 1)
 
 n, m = map(int, input().split())
 arr = [list(map(ord, input())) for _ in range(n)]
-visit = {arr[0][0]}
 ans = 1
 
-def dfs(r, c, k):
-    global ans
-    ans = max(ans , k)
+stack = [(0, 0, 1, {arr[0][0]})]
+while stack:
+    r, c, cnt, visit = stack.pop()
+    ans = max(ans, cnt)
 
     for i in range(4):
         nr, nc = r + dr[i], c + dc[i]
         if nr < 0 or nr >= n or nc < 0 or nc >= m or arr[nr][nc] in visit: continue
-        visit.add(arr[nr][nc])
-        dfs(nr, nc, k+1)
-        visit.discard(arr[nr][nc])
-        
-dfs(0, 0, 1)
+        new_visit = visit.copy()
+        new_visit.add(arr[nr][nc])
+        stack.append((nr, nc, cnt+1, new_visit))
 
 print(ans)
