f424e21cc5f970858949d0139ae5c59a9c02b00f
uttamapaksa <122511728+uttamapaksa@users.noreply.github.com>
2023-12-01 19:11:13 +0900

    [Gold IV] Title: 가장 긴 바이토닉 부분 수열, Time: 140 ms, Memory: 110616 KB -BaekjoonHub

--- 백준/Gold/11054. 가장 긴 바이토닉 부분 수열/가장 긴 바이토닉 부분 수열.py
+++ 백준/Gold/11054. 가장 긴 바이토닉 부분 수열/가장 긴 바이토닉 부분 수열.py
@@ -4,7 +4,6 @@ left = [1] * n
 right = [1] * n
 
 for i in range(n):
-    tmp = 0
     for j in range(0, i):
         if arr[i] <= arr[j]:
             continue
@@ -12,7 +11,6 @@ for i in range(n):
             left[i] = max(left[i], left[j]+1)
 
 for i in range(n-1, -1, -1):
-    tmp = 0
     for j in range(n-1, i, -1):
         if arr[i] <= arr[j]:
             continue
@@ -21,6 +19,6 @@ for i in range(n-1, -1, -1):
 
 ans = 0
 for i in range(n):
-   ans = max(ans, left[i] + right[i])
+    ans = max(ans, left[i] + right[i])
 
 print(ans - 1)
