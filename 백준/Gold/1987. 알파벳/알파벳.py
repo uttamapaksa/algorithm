3fb041d64a7f5442161f1f4af692b0b3f4181019
uttamapaksa <122511728+uttamapaksa@users.noreply.github.com>
2023-12-04 18:17:00 +0900

    [Gold IV] Title: 알파벳, Time: 284 ms, Memory: 110616 KB -BaekjoonHub

--- 백준/Gold/1987. 알파벳/알파벳.py
+++ 백준/Gold/1987. 알파벳/알파벳.py
@@ -1,17 +1,21 @@
-delta = ((-1, 0), (0, -1), (1, 0), (0, 1))
-
 n, m = map(int, input().split())
 arr = [[*(input())] for _ in range(n)]
+visit = [[''] * m for _ in range(n)]
+
+delta = ((-1, 0), (0, -1), (1, 0), (0, 1))
+stack = [(0, 0, 1, arr[0][0])]
 ans = 1
 
-stack = [(0, 0, 1, arr[0][0])]
 while stack:
-    r, c, cnt, visit = stack.pop()
+    r, c, cnt, path = stack.pop()
     ans = max(ans, cnt)
 
     for dr, dc in delta:
         nr, nc = r + dr, c + dc
-        if nr < 0 or nr >= n or nc < 0 or nc >= m or arr[nr][nc] in visit: continue
-        stack.append((nr, nc, cnt+1, visit + arr[nr][nc]))
+        if nr < 0 or nr >= n or nc < 0 or nc >= m or arr[nr][nc] in path: continue
+        new_path = path + arr[nr][nc]
+        if visit[nr][nc] != new_path:
+            visit[nr][nc] = new_path
+            stack.append((nr, nc, cnt+1, new_path))
 
 print(ans)
