3f86fa73c8abf883c5feb9ffcc174ea8a7294acf
uttamapaksa <122511728+uttamapaksa@users.noreply.github.com>
2023-12-04 17:44:59 +0900

    [Gold IV] Title: 알파벳, Time: 5212 ms, Memory: 112084 KB -BaekjoonHub

--- 백준/Gold/1987. 알파벳/알파벳.py
+++ 백준/Gold/1987. 알파벳/알파벳.py
@@ -1,20 +1,17 @@
-dr = (-1, 0, 1, 0)
-dc = (0, -1, 0, 1)
+delta = ((-1, 0), (0, -1), (1, 0), (0, 1))
 
 n, m = map(int, input().split())
-arr = [list(map(ord, input())) for _ in range(n)]
+arr = [[*(input())] for _ in range(n)]
 ans = 1
 
-stack = [(0, 0, 1, {arr[0][0]})]
+stack = [(0, 0, 1, arr[0][0])]
 while stack:
     r, c, cnt, visit = stack.pop()
     ans = max(ans, cnt)
 
-    for i in range(4):
-        nr, nc = r + dr[i], c + dc[i]
+    for dr, dc in delta:
+        nr, nc = r + dr, c + dc
         if nr < 0 or nr >= n or nc < 0 or nc >= m or arr[nr][nc] in visit: continue
-        new_visit = visit.copy()
-        new_visit.add(arr[nr][nc])
-        stack.append((nr, nc, cnt+1, new_visit))
+        stack.append((nr, nc, cnt+1, visit + arr[nr][nc]))
 
 print(ans)
