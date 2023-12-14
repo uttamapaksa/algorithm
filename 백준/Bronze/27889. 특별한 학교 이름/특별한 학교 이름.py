7bb48763ad29b885e25d2ad80610c32295ea5da6
uttamapaksa <122511728+uttamapaksa@users.noreply.github.com>
2023-12-15 00:16:34 +0900

    [Bronze V] Title: 특별한 학교 이름, Time: 108 ms, Memory: 108080 KB -BaekjoonHub

---
+++ 백준/Bronze/27889. 특별한 학교 이름/특별한 학교 이름.py
@@ -0,0 +1,8 @@
+answer = {
+    'NLCS': 'North London Collegiate School',
+    'BHA': 'Branksome Hall Asia',
+    'KIS': 'Korea International School',
+    'SJA': 'St. Johnsbury Academy',
+}
+
+print(answer[input()])
