8e8e31bd89a3f498a577fac6b45cebf790fc0008
uttamapaksa <122511728+uttamapaksa@users.noreply.github.com>
2024-01-06 11:30:09 +0900

    [Silver II] Title: 부분수열의 합, Time: 264 ms, Memory: 112272 KB -BaekjoonHub

--- 백준/Silver/1182. 부분수열의 합/부분수열의 합.py
+++ 백준/Silver/1182. 부분수열의 합/부분수열의 합.py
@@ -1,21 +1,18 @@
 N, S = map(int, input().split())
 arr = list(map(int, input().split()))
-visit = [0] * N
 ans = 0
 
 
-def dfs(sumv, k):
+def dfs(sumv, k, c):
     global ans 
-    if sumv == S and k > 0: # 처음부터 같을 경우를 배제(k > 0)
-        ans += 1 # 뒤에서 또 성립될 수 있으므로 return 하지 않음
-    if k == N: return
+    if k == N: # 끝까지 모든 경우 완전탐색
+        if sumv == S and c > 0: # 부분집합이 공집합인 경우를 배제(c > 0)
+            ans += 1
+        return
     
-    for i in range(k, N):
-        if visit[i]: continue
-        visit[i] = 1
-        dfs(sumv + arr[i], i+1)
-        visit[i] = 0
-    
+    dfs(sumv + arr[k], k+1, c+1)
+    dfs(sumv, k+1, c)
 
-dfs(0, 0)
+
+dfs(0, 0, 0)
 print(ans)
