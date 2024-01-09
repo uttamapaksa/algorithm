328980f8eb78b433ccca1ff21dafd1da9710e640
uttamapaksa <122511728+uttamapaksa@users.noreply.github.com>
2024-01-09 10:21:57 +0900

    [Silver II] Title: 가장 긴 증가하는 부분 수열, Time: 120 ms, Memory: 109240 KB -BaekjoonHub

--- 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.py
+++ 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.py
@@ -1,16 +1,11 @@
-import sys; input = sys.stdin.readline
-from heapq import heappop, heappush
-
 N = int(input())
-arr = [*map(int, input().split())]
-dp = [-1]
+arr = list(map(int, input().split()))
+dp = [1] * N
 
 for i in range(1, N):
     v = arr[i]
-    heap = [0]
-    for j in range(i):
+    for j in range(0, i):
         if arr[j] < v:
-            heappush(heap, dp[j])
-    dp.append(heappop(heap) - 1)
+            dp[i] = max(dp[i], dp[j]+1)
 
-print(-min(dp))
+print(max(dp))
