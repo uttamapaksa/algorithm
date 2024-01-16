472e40ca66d618b196c52b84149962409541a208
uttamapaksa <122511728+uttamapaksa@users.noreply.github.com>
2024-01-16 09:05:16 +0900

    [Gold III] Title: 주사위 굴리기 2, Time: 120 ms, Memory: 109240 KB -BaekjoonHub

--- 백준/Gold/23288. 주사위 굴리기 2/주사위 굴리기 2.py
+++ 백준/Gold/23288. 주사위 굴리기 2/주사위 굴리기 2.py
@@ -1,55 +1,65 @@
 def rotate_dice():
-    num, nd = arr[x][y], d
-
-    if bot > num:
-        nd = (nd+1) % 4
-    elif bot < num:
-        nd = (nd+3) % 4
-    return nd
-
-
-def get_score():
-    num = arr[x][y]
-    stack = [(x, y)]
-    visit = {(x, y)}
-
-    while stack:
-        r, c = stack.pop()
-        for dr, dc in delta:
-            nr, nc = r + dr, c + dc
-            if 0 <= nr < N and 0 <= nc < M and arr[nr][nc] == num and (nr, nc) not in visit:
-                stack.append((nr, nc))
-                visit.add((nr, nc))       
-    return num * len(visit)
+    global d
+    if bot > arr[x][y]:
+        d = (d+1) % 4
+    elif bot < arr[x][y]:
+        d = (d+3) % 4
 
 
 def move_dice():
-    nx, ny, nd = x + delta[d][0], y + delta[d][1], d
-    if nx < 0 or nx >= N or ny < 0 or ny >= M:
-        nd = (nd+2) % 4
-        nx, ny = x + delta[nd][0], y + delta[nd][1]
+    global top, bot, left, right, up, down, x, y, d, ans
+    # x, y, d
+    dx, dy = delta[d]
+    x, y = x + dx, y + dy
+    if x < 0 or x >= N or y < 0 or y >= M:
+        x, y = x - dx*2, y - dy*2
+        d = (d+2) % 4
 
-    if not nd:
-        ntop, nbot, nleft, nright, nup, ndown = left, right, bot, top, up, down
-    elif nd == 1:
-        ntop, nbot, nleft, nright, nup, ndown = up, down, left, right, bot, top
-    elif nd == 2:
-        ntop, nbot, nleft, nright, nup, ndown = right, left, top, bot, up, down
+    # top, bot, left, right, up, down
+    if not d:
+        top, bot, left, right, up, down = left, right, bot, top, up, down
+    elif d == 1:
+        top, bot, left, right, up, down = up, down, left, right, bot, top
+    elif d == 2:
+        top, bot, left, right, up, down = right, left, top, bot, up, down
     else:
-        ntop, nbot, nleft, nright, nup, ndown = down, up, left, right, top, bot
+        top, bot, left, right, up, down = down, up, left, right, top, bot
 
-    return ntop, nbot, nleft, nright, nup, ndown, nx, ny, nd
+    # score
+    ans += score_table[x][y] 
+
+
+def make_score_table():
+    for x in range(N):
+        for y in range(M):
+            if not score_table[x][y]:
+                num = arr[x][y]
+                stack = [(x, y)]
+                visit = {(x, y)}
+
+                while stack:
+                    r, c = stack.pop()
+                    for dr, dc in delta:
+                        nr, nc = r + dr, c + dc
+                        if 0 <= nr < N and 0 <= nc < M and arr[nr][nc] == num and (nr, nc) not in visit:
+                            stack.append((nr, nc))
+                            visit.add((nr, nc))
+                
+                score = num * len(visit)
+                for r, c in visit:
+                    score_table[r][c] = score
 
 
 N, M, K = map(int, input().split())
 arr = [list(map(int, input().split())) for _ in range(N)]
+score_table = [[0] * M for _ in range(N)]
 x = y = d = ans = 0
 top, bot, left, right, up, down = 1, 6, 4, 3, 2, 5
-delta = ((0, 1), (1, 0), (0, -1), (-1, 0)) # E S W N
+delta = ((0, 1), (1, 0), (0, -1), (-1, 0))
 
+make_score_table()
 for _ in range(K):
-    top, bot, left, right, up, down, x, y, d = move_dice()
-    ans += get_score()
-    d = rotate_dice()
+    move_dice()
+    rotate_dice()
 
 print(ans)
