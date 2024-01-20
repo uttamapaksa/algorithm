dacfd5c0f706fb425eb106b2ce084087623c91ef
uttamapaksa <122511728+uttamapaksa@users.noreply.github.com>
2024-01-20 17:45:52 +0900

    [Silver V] Title: 너의 평점은, Time: 60 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/25206. 너의 평점은/너의 평점은.py
@@ -0,0 +1,21 @@
+score = {
+    'A+': 4.5,
+    'A0': 4.0,
+    'B+': 3.5,
+    'B0': 3.0,
+    'C+': 2.5,
+    'C0': 2.0,
+    'D+': 1.5,
+    'D0': 1.0,
+    'F': 0.0
+}
+
+cnt = 0
+total = 0
+for _ in range(20):
+    [x, time, grade] = input().split()
+    if grade in score:
+        total += score[grade] * float(time)
+        cnt += float(time)
+
+print(round(total/cnt, 6))
