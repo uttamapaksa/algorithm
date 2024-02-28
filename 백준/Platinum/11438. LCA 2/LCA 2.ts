02302442463e1617d6698d252cd5f86d024e6c95
uttamapaksa <122511728+uttamapaksa@users.noreply.github.com>
2024-02-28 14:14:39 +0900

    [Platinum V] Title: LCA 2, Time: 972 ms, Memory: 139472 KB -BaekjoonHub

--- 백준/Platinum/11438. LCA 2/LCA 2.ts
+++ 백준/Platinum/11438. LCA 2/LCA 2.ts
@@ -1,10 +1,15 @@
-function dfs(p: number, c: number, d: number) {
-  if (par[c][0]) return
-
-  depth[c] = d
-  par[c][0] = p
-  for (const x of graph[c]) {
-    dfs(c, x, d+1)
+function dfs(p: number, d: number) {
+  const stack = [[p, d]]
+  depth[p] = d
+  par[0][p] = p
+  while (stack.length) {
+    const [p, d] = stack.pop() as number[]
+    for (const c of graph[p]) {
+      if (par[0][c]) continue
+      depth[c] = d+1
+      par[0][c] = p
+      stack.push([c, d+1])
+    }
   }
 }
 
@@ -12,7 +17,7 @@ function dfs(p: number, c: number, d: number) {
 function parentDP() {
   for (let j=1; j<LOG; j++) {
     for (let i=1; i<N+1; i++) {
-      par[i][j] = par[par[i][j-1]][j-1]
+      par[j][i] = par[j-1][par[j-1][i]]
     }
   }
 }
@@ -22,7 +27,7 @@ function LCA(a: number, b: number): number {
   if (depth[a] < depth[b]) [a, b] = [b, a] // depth[a] > depth[b]
   for (let i=LOG-1; i>=0; i--) {
     if ((depth[a] - depth[b]) >= (1 << i) ) {
-      a = par[a][i]
+      a = par[i][a]
     }
   }
   
@@ -30,23 +35,23 @@ function LCA(a: number, b: number): number {
     return a
   }
   for (let i=LOG-1; i>=0; i--) {
-    if (par[a][i] !== par[b][i]) {
-      a = par[a][i]
-      b = par[b][i]
+    if (par[i][a] !== par[i][b]) {
+      a = par[i][a]
+      b = par[i][b]
     }
   }
-  return par[a][0]
+  return par[0][a]
 }
 
 
 const [[N], ...input]: number[][] = require('fs').readFileSync(process.platform==='linux'?0:'input.txt').toString().trim().split('\n').map((x:string)=>x.trim().split(' ').map(Number))
 const [edges, [M], queries] = [input.slice(0, N-1), input[N-1], input.slice(N)]
-const LOG = 18 // 2 ** 17 > N
 const graph: number[][] = Array.from({ length: N+1 }, () => [])
-const par: number[][] = Array.from({ length: N+1 }, () => Array(LOG).fill(0))
 edges.forEach(([u, v]) => (graph[u].push(v), graph[v].push(u)))
+const LOG = 18 // 2 ** 17 > N
+const par: number[][] = Array.from({ length: LOG }, () => Array(N+1).fill(0))
 const depth = Array(N+1).fill(0)
-dfs(1, 1, 0)
+dfs(1, 0)
 parentDP()
 
 let ans = '';
