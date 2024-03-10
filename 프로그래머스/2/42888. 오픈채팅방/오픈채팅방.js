7f5c4440f3cb741d3afb05d63f2268dfc8e1e15e
uttamapaksa <122511728+uttamapaksa@users.noreply.github.com>
2024-03-10 11:59:54 +0900

    [level 2] Title: 오픈채팅방, Time: 125.76 ms, Memory: 98.4 MB -BaekjoonHub

--- 프로그래머스/2/42888. 오픈채팅방/오픈채팅방.js
+++ 프로그래머스/2/42888. 오픈채팅방/오픈채팅방.js
@@ -1,21 +1,23 @@
 function solution(records) {
-    const user = {}
-    let ans = []
-    
-    records.forEach(record => {
-        const [oper, id, name] = record.split(' ')      
-        if (oper == 'Enter' || oper == 'Change') {
-            user[id] = name
-        }
-    })
+    const ans = []
+    const names = {}
+    const messages = []
     
     records.forEach(record => {
         const [oper, id, name] = record.split(' ')      
         if (oper == 'Enter') {
-            ans.push(`${user[id]}님이 들어왔습니다.`)
-        } else if (oper == 'Leave') {
-            ans.push(`${user[id]}님이 나갔습니다.`)
+            names[id] = name
+            messages.push([id, '님이 들어왔습니다.'])
+        } else if (oper == 'Change') {
+            names[id] = name
+        } else {
+            messages.push([id, '님이 나갔습니다.'])
         }
+    })
+    
+    messages.forEach(message => {
+        const [id, mes] = message
+        ans.push(`${names[id]}` + mes)
     })
 
     return ans;
