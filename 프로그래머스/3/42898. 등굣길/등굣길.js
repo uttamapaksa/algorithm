8fe270348ff92c4571f4028335148f80609f9d70
uttamapaksa <122511728+uttamapaksa@users.noreply.github.com>
2024-03-17 13:21:07 +0900

    [level 3] Title: 등굣길, Time: 1.28 ms, Memory: 33 MB -BaekjoonHub

--- 프로그래머스/3/42898. 등굣길/등굣길.js
+++ 프로그래머스/3/42898. 등굣길/등굣길.js
@@ -1,30 +1,16 @@
 function solution(m, n, puddles) {
-    const arr = Array.from({length: n}, () => Array(m).fill(0));
-    for (const [x, y] of puddles) {
-        arr[y-1][x-1] = -1;
-    }
+    const arr = Array.from({length: n+1}, () => Array(m+1).fill(0));
+    for (const [x, y] of puddles) { arr[y][x] = -1; }
+    arr[1][1] = 1;
     
-    for (let r=0; r<n; r++) {
-        if (arr[r][0] === -1) break;
-        arr[r][0] = 1;
-    }
-    for (let c=0; c<m; c++) {
-        if (arr[0][c] === -1) break;
-        arr[0][c] = 1;
-    }
-    
-    for (let r=1; r<n; r++) {
-        for (let c=1; c<m; c++) {
+    for (let r=1; r<=n; r++) {
+        for (let c=1; c<=m; c++) {
             if (arr[r][c] === -1) continue;
-            if (arr[r-1][c] !== -1) {
-                arr[r][c] += arr[r-1][c];
-            }
-            if (arr[r][c-1] !== -1) {
-                arr[r][c] += arr[r][c-1];
-            }
+            if (arr[r-1][c] !== -1) { arr[r][c] += arr[r-1][c]; }
+            if (arr[r][c-1] !== -1) { arr[r][c] += arr[r][c-1]; }
             arr[r][c] %= 1000000007;
         }
     }
     
-    return arr[n-1][m-1];
+    return arr[n][m];
 }
