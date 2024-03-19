856b6a9564bb8af0ac3c7ec998aa1eb0fbc4f210
uttamapaksa <122511728+uttamapaksa@users.noreply.github.com>
2024-03-19 21:13:23 +0900

    [Gold III] Title: 말이 되고픈 원숭이, Time: 884 ms, Memory: 177844 KB -BaekjoonHub

--- 백준/Gold/1600. 말이 되고픈 원숭이/말이 되고픈 원숭이.ts
+++ 백준/Gold/1600. 말이 되고픈 원숭이/말이 되고픈 원숭이.ts
@@ -1,26 +1,30 @@
 const [[K], [C, R], ...arr]: number[][] = require('fs').readFileSync(process.platform==='linux'?0:'input.txt').toString().trim().split('\n').map((x:string)=>x.split(' ').map(Number));
 const pawn = [[-1, 0], [0, -1], [1, 0], [0, 1]];
 const knight = [[-2, -1], [-1, -2], [2, -1], [1, -2], [2, 1], [1, 2], [-2, 1], [-1, 2]];
-const visit = Array.from({length: R}, ()=>Array.from({length: C}, ()=>Array(K+1).fill(0)));
+const visit = Array.from({length: R}, ()=>Array.from({length: C}, ()=>Array(K+1).fill(0))); // [r][c][k]
 visit[0][0][K] = 1;
 const queue: number[][] = [[0, 0, K]];
 queue.push([0, 0, K]);
 let front = 0;
+let answer = 0;
 
 while (front < queue.length) {
   const [r, c, t] = queue[front++] as number[];
-
-  if (r === R && c === C) break;
-
+  // arrive
+  if (r === R-1 && c === C-1) {
+    answer = visit[r][c][t];
+    break;
+  }
+  // monkey move
   for (const [dr, dc] of pawn) {
     const [nr, nc] = [r + dr, c + dc];
     if (nr < 0 || nr >= R || nc < 0 || nc >= C || arr[nr][nc] || visit[nr][nc][t]) continue;
     visit[nr][nc][t] = visit[r][c][t] + 1;
     queue.push([nr, nc, t]);
   }
-
+  // knight move is impossible
   if (!t) continue;
-
+  // knight move
   for (const [dr, dc] of knight) {
     const [nr, nc] = [r + dr, c + dc];
     if (nr < 0 || nr >= R || nc < 0 || nc >= C || arr[nr][nc] || visit[nr][nc][t-1]) continue;
@@ -29,5 +33,4 @@ while (front < queue.length) {
   }
 }
 
-const arrive = visit[R-1][C-1].filter((x: number) => x > 0);
-console.log(arrive.length ? Math.min(...arrive) - 1 : -1);
+console.log(--answer);
