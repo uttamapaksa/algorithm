4957e43d23aa74b4f2e8e9700b684db4618b5022
uttamapaksa <122511728+uttamapaksa@users.noreply.github.com>
2024-04-07 18:24:31 +0900

    [level 2] Title: [카카오 인턴] 수식 최대화, Time: 2.11 ms, Memory: 82.6 MB -BaekjoonHub

---
+++ 프로그래머스/2/67257. ［카카오 인턴］ 수식 최대화/［카카오 인턴］ 수식 최대화.java
@@ -0,0 +1,72 @@
+import java.util.*;
+
+class Solution {
+    static int N;
+    static int[] P;
+    static long[] numsCopy;
+    static ArrayList<Long> nums = new ArrayList<>();
+    static ArrayList<String> opers = new ArrayList<>();
+
+    static int find(int x) {
+        if (P[x] != x) {
+            P[x] = find(P[x]);
+        }
+        return P[x];
+    }
+
+    static void union(int x, int y, long v) {
+        int a = find(x);
+        int b = find(y);
+        if (a < b) {
+            P[b] = a;
+            numsCopy[a] = v;
+        } else {
+            P[a] = b;
+            numsCopy[b] = v;
+        }
+    } 
+    
+    public long solution(String expression) {
+        String word = "";
+        for (String x: expression.split("")) {
+            if (x.equals("+") || x.equals("-") || x.equals("*")) {
+                opers.add(x);
+                nums.add(Long.parseLong(word));
+                word = "";
+            } else {
+                word += x;
+            }
+        }
+        nums.add(Long.parseLong(word));
+        
+        N = nums.size();
+        P = new int[N];
+        numsCopy = new long[N];
+        
+        long answer = 0L;
+        for (String[] o: new String[][]{{"*","+","-"}, {"*","-","+"}, {"+","*","-"}, {"+","-","*"}, {"-","*","+"}, {"-","+","*"}}) {
+            for (int i=0; i<N; i++) {
+                P[i] = i;
+            }
+            for (int i=0; i<N; i++) {
+                numsCopy[i] = nums.get(i);
+            }
+            for (int i=0; i<3; i++) {
+                for (int j=0; j<N-1; j++) {
+                    if (!opers.get(j).equals(o[i])) continue;
+                    
+                    long result;
+                    if (opers.get(j).equals("*")) result = numsCopy[find(j)] * numsCopy[find(j+1)];
+                    else if (opers.get(j).equals("+")) result = numsCopy[find(j)] + numsCopy[find(j+1)];
+                    else if (opers.get(j).equals("-")) result = numsCopy[find(j)] - numsCopy[find(j+1)];
+                    else continue;
+                    union(j, j + 1, result);
+                }   
+            }
+            answer = Math.max(answer, Math.abs(numsCopy[0]));
+        }
+        
+        return answer;
+    }
+}
+
