d68cf6b97699230d83956f650d587c9c1565ee50
uttamapaksa <122511728+uttamapaksa@users.noreply.github.com>
2024-04-20 10:27:17 +0900

    [Platinum V] Title: 행성 터널, Time: 2368 ms, Memory: 184896 KB -BaekjoonHub

---
+++ 백준/Platinum/2887. 행성 터널/행성 터널.py
@@ -0,0 +1,52 @@
+from heapq import heappush, heappop
+
+# find
+def find(x):
+    if P[x] != x:
+        P[x] = find(P[x])
+    return P[x]
+
+# union
+def union(w, x, y):
+    a, b = find(x), find(y)
+    if a == b:
+        return 0
+    if a > b:
+        a, b = b, a
+    P[b] = a
+    return w
+
+# static
+xArr = []
+yArr = []
+zArr = []
+answer = 0
+
+# input
+N = int(input())
+P = list(range(N))
+for planet in range(N):
+    x, y, z = map(int, input().split())
+    xArr.append((x, planet))
+    yArr.append((y, planet))
+    zArr.append((z, planet))
+xArr.sort()
+yArr.sort()
+zArr.sort()
+
+# diff heap
+heap = []
+for planet in range(N-1):
+    # heappush w, u, v
+    heappush(heap, (xArr[planet+1][0] - xArr[planet][0], xArr[planet+1][1], xArr[planet][1]))
+    heappush(heap, (yArr[planet+1][0] - yArr[planet][0], yArr[planet+1][1], yArr[planet][1]))
+    heappush(heap, (zArr[planet+1][0] - zArr[planet][0], zArr[planet+1][1], zArr[planet][1]))
+
+# kruskal
+answer = 0
+while heap:
+    w, u, v = heappop(heap)
+    answer += union(w, u, v)
+
+# output
+print(answer)
