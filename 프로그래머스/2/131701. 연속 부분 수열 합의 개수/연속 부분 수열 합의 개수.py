4c840d9dc20417695d443e85c2abb6533966f220
uttamapaksa <122511728+uttamapaksa@users.noreply.github.com>
2024-04-29 10:31:39 +0900

    [level 2] Title: 연속 부분 수열 합의 개수, Time: 162.11 ms, Memory: 43.7 MB -BaekjoonHub

--- 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.py
+++ 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.py
@@ -1,15 +1,19 @@
 def solution(elements):
     N = len(elements)
-    elements = elements + elements
-    sums = set()
+    sumv = sum(elements)
+    sums = {sumv}
     
-    for i in range(1, N+1):
+    elements = elements + elements
+    
+    for i in range(1, N//2 + 2):
         init = sum(elements[:i])
         sums.add(init)
+        sums.add(sumv - init)
         for j in range(N-1):
             init += elements[i+j]
             init -= elements[j]
             sums.add(init)
+            sums.add(sumv - init)
     
     answer = len(sums)
     return answer
