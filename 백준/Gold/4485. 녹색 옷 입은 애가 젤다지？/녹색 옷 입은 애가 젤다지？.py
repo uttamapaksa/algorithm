a20e247e71598140e47adc935762080c4eb392ba
uttamapaksa <122511728+uttamapaksa@users.noreply.github.com>
2024-04-30 11:38:18 +0900

    [Gold IV] Title: 녹색 옷 입은 애가 젤다지?, Time: 216 ms, Memory: 114472 KB -BaekjoonHub

--- 백준/Gold/4485. 녹색 옷 입은 애가 젤다지？/녹색 옷 입은 애가 젤다지？.py
+++ 백준/Gold/4485. 녹색 옷 입은 애가 젤다지？/녹색 옷 입은 애가 젤다지？.py
@@ -1,3 +1,5 @@
+from collections import deque
+
 delta = ((-1, 0), (0, -1), (1, 0), (0, 1))
 i = 0
 
@@ -10,16 +12,16 @@ while True:
     dp = [[2500] * N for _ in range(N)]
 
     dp[0][0] = arr[0][0]
-    stack = [(0, 0)]
-    while stack:
-        r, c = stack.pop()
+    queue = deque([(0, 0)])
+    while queue:
+        r, c = queue.popleft()
         for dr, dc in delta:
             nr, nc = r + dr, c + dc
-            if nr < 0 or nr >= N or nc < 0 or nc >= N: continue  # index
+            if nr < 0 or nr >= N or nc < 0 or nc >= N: continue  # index error
             if dp[-1][-1] <= dp[r][c] + arr[nr][nc]: continue  # smaller than goal
             if dp[nr][nc] <= dp[r][c] + arr[nr][nc]: continue  # smaller than next
             dp[nr][nc] = dp[r][c] + arr[nr][nc]
-            stack.append((nr, nc))
+            queue.append((nr, nc))
 
     i += 1
     print(f'Problem {i}: {dp[-1][-1]}')
