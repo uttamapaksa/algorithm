d82a663f5ed96df504fdef8a84f5d96066d43a41
Junseop Kim <122511728+uttamapaksa@users.noreply.github.com>
2024-05-10 12:08:40 +0900

    [level 2] Title: 2개 이하로 다른 비트, Time: 335.50 ms, Memory: 23.9 MB -BaekjoonHub

--- 프로그래머스/2/77885. 2개 이하로 다른 비트/2개 이하로 다른 비트.py
+++ 프로그래머스/2/77885. 2개 이하로 다른 비트/2개 이하로 다른 비트.py
@@ -1,16 +1,15 @@
 def solution(numbers):
     ans = []
+    
     for num in numbers:
-        N = len(bin(num)) - 2
-        for i in range(N-1):
-            if num & (1 << i) == 0:
-                num |= (1 << i)
+        i = 0
+        while True:
+            if not num & (1 << i):
+                num += 1 << i
                 if i:
                     num -= 1 << (i-1)
                 break
-        else:
-            num += 1 << N
-            num -= 1 << (N-1)
+            i += 1
         ans.append(num)
     
     return ans
