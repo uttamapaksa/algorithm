0e85deb1ad977e7ed11a36f1eed163aec7bb0613
Junseop Kim <122511728+uttamapaksa@users.noreply.github.com>
2024-05-14 12:10:50 +0900

    [Gold IV] Title: 이모티콘, Time: 152 ms, Memory: 120364 KB -BaekjoonHub

--- 백준/Gold/14226. 이모티콘/이모티콘.py
+++ 백준/Gold/14226. 이모티콘/이모티콘.py
@@ -1,26 +1,21 @@
 from collections import deque
 
-inf = 1 << 11
-s = int(input())
-dp = [[inf] * (s+1) for _ in range(s+1)]
 
-q = deque([(1, 1)])
-dp[1][1] = 1  # node, copy
-while q:
-    u, c = q.popleft()
-    v = u - 1
-    if 0 < v <= s:
-        if dp[v][c] > dp[u][c] + 1:
-            dp[v][c] = dp[u][c] + 1
-            q.append((v, c))
-    v = u + c
-    if 0 < v <= s:
-        if dp[v][c] > dp[u][c] + 1:
-            dp[v][c] = dp[u][c] + 1
-            q.append((v, c))
-    if u != c:
-        if dp[u][u] > dp[u][c] + 1:
-            dp[u][u] = dp[u][c] + 1
-            q.append((u, u))
+def sol(s):
+    inf = 1 << 11
+    dp = [[inf] * (s+1) for _ in range(s+1)]
 
-print(min(dp[s]))
+    q = deque([(1, 1)])
+    dp[1][1] = 1  # node, copy
+    while q:
+        u, uc = q.popleft()
+        for v, vc in ((u-1, uc), (u+uc, uc), (u, u)):
+            if v < 1 or v > s: continue
+            if v == s:
+                return dp[u][uc] + 1
+            if dp[v][vc] > dp[u][uc] + 1:
+                dp[v][vc] = dp[u][uc] + 1
+                q.append((v, vc))
+
+
+print(sol(int(input())))
