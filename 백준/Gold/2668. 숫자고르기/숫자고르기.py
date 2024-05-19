fdc6f6c277b5f44959f82b5e0e5d29163e7fb90f
Junseop Kim <122511728+uttamapaksa@users.noreply.github.com>
2024-05-19 13:34:51 +0900

    [Gold V] Title: 숫자고르기, Time: 108 ms, Memory: 109240 KB -BaekjoonHub

---
+++ 백준/Gold/2668. 숫자고르기/숫자고르기.py
@@ -0,0 +1,22 @@
+def cycle(r):
+    global ans
+    visit = set()
+    u = r
+    while u not in visit:
+        visit.add(u)
+        u = arr[u]
+    if r == u:
+        ans |= visit
+
+n = int(input())
+arr = [0] + [int(input()) for _ in range(n)]
+
+ans = set()
+
+for i in range(1, n+1):
+    if i in ans: continue
+    cycle(i)
+
+print(len(ans))
+for v in sorted(ans):
+    print(v)
