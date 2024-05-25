44b24a62dd09dfd788820ddc1d921cd7bec7746b
Junseop Kim <122511728+uttamapaksa@users.noreply.github.com>
2024-05-26 08:59:38 +0900

    [Gold II] Title: 빵집, Time: 628 ms, Memory: 156428 KB -BaekjoonHub

---
+++ 백준/Gold/3109. 빵집/빵집.py
@@ -0,0 +1,25 @@
+def toInt(x):
+    if x == '.': 
+        return 0
+    return 1
+
+N, M = map(int, input().split())
+arr = [[*map(toInt, [*input()])] for _ in range(N)]
+ans = 0
+
+for r in range(N):
+    stack = [(r, 0)]
+    while stack:
+        r, c = stack.pop()
+        if arr[r][c]:
+            continue
+        arr[r][c] = 1
+        if c == M-1:
+            ans += 1
+            break
+        for nr, nc in ((r+1, c+1), (r, c+1), (r-1, c+1)):
+            if 0 <= nr < N and 0 <= nc < M and not arr[nr][nc]:
+                stack.append((nr, nc))
+                stack.append((nr, nc))
+
+print(ans)
