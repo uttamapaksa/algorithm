e900961dd7b4f7b54784a92e3b6956763702dfb6
Junseop Kim <122511728+uttamapaksa@users.noreply.github.com>
2024-05-29 07:02:18 +0900

    [level 2] Title: 조이스틱, Time: 0.02 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/42860. 조이스틱/조이스틱.py
@@ -0,0 +1,19 @@
+def solution(name):
+    n = len(name)
+    ans = 0
+
+    updown = [min(ord(char) - ord('A'), ord('Z') - ord(char) + 1) for char in name]
+    ans += sum(updown)
+
+    leftright = n - 1
+    for i in range(n):
+        next = i + 1
+        while next < n and name[next] == 'A':
+            next += 1
+        right_back_left = i + i + n - next 
+        left_back_rigth = n - next + n - next + i
+        leftright = min(leftright, right_back_left, left_back_rigth)
+    ans += leftright
+
+    return ans
+
