62d71323a30e7ec2b10cc6edf851c066f051e8e7
Junseop Kim <122511728+uttamapaksa@users.noreply.github.com>
2024-06-10 13:50:42 +0900

    [level 3] Title: 이중우선순위큐, Time: 0.03 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.py
+++ 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.py
@@ -1,26 +1,35 @@
-from heapq import heapify, heappush, heappop
+from heapq import heappush, heappop
 
 def solution(operations):
+    n = len(operations)
+    deleted = [0] * n
     minh = []
     maxh = []
     
-    for operation in operations:
-        oper, num = operation.split()
+    for i in range(n):
+        oper, num = operations[i].split()
         num = int(num)
         
         if oper == 'I':
-            heappush(minh, num)
-            heappush(maxh, -num)
+            heappush(minh, (num, i))
+            heappush(maxh, (-num, i))
+        elif num == 1:
+            while maxh and deleted[maxh[0][1]]:
+                heappop(maxh)
+            if maxh:
+                deleted[heappop(maxh)[1]] = 1
         else:
-            if not minh: continue
-            if num == 1:
-                minh.remove(-heappop(maxh))
-                heapify(minh)
-            else:
-                maxh.remove(-heappop(minh))
-                heapify(maxh)
+            while minh and deleted[minh[0][1]]:
+                heappop(minh)
+            if minh:
+                deleted[heappop(minh)[1]] = 1
+                
+    while maxh and deleted[maxh[0][1]]:
+        heappop(maxh)
+    while minh and deleted[minh[0][1]]:
+        heappop(minh)
     
     if not minh:
         return [0, 0]
     else:
-        return [-maxh[0], minh[0]]
+        return [-maxh[0][0], minh[0][0]]
