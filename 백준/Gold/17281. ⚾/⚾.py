4f7ad247e807019d90cdf33dbd94ad6a1eae6877
Junseop Kim <122511728+uttamapaksa@users.noreply.github.com>
2024-06-12 23:11:14 +0900

    [Gold IV] Title: ⚾, Time: 1352 ms, Memory: 113648 KB -BaekjoonHub

--- 백준/Gold/17281. ⚾/⚾.py
+++ 백준/Gold/17281. ⚾/⚾.py
@@ -1,17 +1,12 @@
-from itertools import permutations
-
-
-def baseball(hitters, inning):
+def baseball():
     score = 0
-    hitters.insert(3, 0)
     idx = 0
-    while inning < n:
-
+    for inning in range(n):
         out = 0
-        one, two, three = 0, 0, 0 
+        one, two, three = 0, 0, 0
         while out < 3:
             v = arr[inning][hitters[idx]]
-            if v == 0:
+            if not v :
                 out += 1
             elif v == 1:
                 score += three
@@ -29,18 +24,32 @@ def baseball(hitters, inning):
             idx += 1
             if idx == 9:
                 idx = 0
-        inning += 1
 
     global ans
     if ans < score:
         ans = score
 
 
+def perm(k):
+    if k == 3:
+        perm(k+1)
+        return
+    if k == 9:
+        baseball() 
+        return
+    for i in (1, 2, 3, 4, 5, 6, 7, 8):
+        if visit[i]: continue
+        visit[i] = 1
+        hitters[k] = i
+        perm(k+1)
+        visit[i] = 0
+
+
 n = int(input())
 arr = [[*map(int, input().split())] for _ in range(n)]
+visit = [0] * 9
+hitters = [0] * 9
 ans = 0
 
-for hitters in permutations([1, 2, 3, 4, 5, 6, 7, 8]):
-    baseball(list(hitters), 0)
-
+perm(0)
 print(ans)
