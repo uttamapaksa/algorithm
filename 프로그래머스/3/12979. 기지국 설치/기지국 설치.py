04c681920ce3dcb33adb7a2b5251258333f67966
Junseop Kim <122511728+uttamapaksa@users.noreply.github.com>
2024-07-01 20:48:54 +0900

    [level 3] Title: 기지국 설치, Time: 1.70 ms, Memory: 10.5 MB -BaekjoonHub

--- 프로그래머스/3/12979. 기지국 설치/기지국 설치.py
+++ 프로그래머스/3/12979. 기지국 설치/기지국 설치.py
@@ -1,20 +1,13 @@
 def solution(n, stations, w):
-    stations = [(station - w, station + w) for station in stations]
-    area = []
-    s, e = 0, 0
-    for station in stations:
-        if e >= station[0]-1:
-            e = station[1]
-        else:
-            area.append((s, e))
-            s, e = station
-    area.append((s, e))
-    area.append((n+1, n+1))
-    
+    area = w * 2 + 1
+    start = 1
     ans = 0
-    div = 2*w+1
-    for i in range(len(area)-1):
-        diff = area[i+1][0] - area[i][1] - 1
-        ans += (diff-1) // div + 1
+    
+    for station in stations:
+        if station - w - start > 0:
+            ans += (station - w - start - 1) // area + 1 
+        start = station + w + 1
+    if n + 1 - start > 0:
+        ans += (n - start) // area + 1
     
     return ans
