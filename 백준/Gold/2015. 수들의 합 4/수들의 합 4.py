fccf373b1fa03cb3b638b513b8a1fde41a3f33c6
Junseop Kim <122511728+uttamapaksa@users.noreply.github.com>
2024-07-05 15:19:14 +0900

    [Gold IV] Title: 수들의 합 4, Time: 132 ms, Memory: 137256 KB -BaekjoonHub

--- 백준/Gold/2015. 수들의 합 4/수들의 합 4.py
+++ 백준/Gold/2015. 수들의 합 4/수들의 합 4.py
@@ -3,12 +3,10 @@ arr = [0] + [*map(int, input().split())]
 for i in range(2, n+1):
     arr[i] += arr[i-1]
 
-sums = {}
 ans = 0
-for i in range(n+1):
-    v = arr[i]
-    if v-k in sums:
-       ans += sums[v-k]
+sums = {}
+for v in arr:
+    ans += sums.get(v-k, 0)
     sums[v] = sums.get(v, 0) + 1
 
 print(ans)
