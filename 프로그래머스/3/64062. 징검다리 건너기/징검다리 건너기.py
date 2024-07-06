ffd7e5ad206f57d39c566f307b44a5f454388c36
Junseop Kim <122511728+uttamapaksa@users.noreply.github.com>
2024-07-06 09:00:00 +0900

    [level 3] Title: 징검다리 건너기, Time: 64.43 ms, Memory: 20.4 MB -BaekjoonHub

--- 프로그래머스/3/64062. 징검다리 건너기/징검다리 건너기.py
+++ 프로그래머스/3/64062. 징검다리 건너기/징검다리 건너기.py
@@ -1,29 +1,17 @@
+from collections import deque
+
 def solution(stones, k):
-    n = len(stones)
-    stones = [(v, i) for i, v in enumerate(stones)]
-    stones.sort(reverse=True)
-    V = [0] * (n)
-    SE = [[i, i] for i in range(n)]  # [start, end][]
-    cnt = 0
+    q = deque()
+    mxa = []
     
-    while stones:
-        mn, idx = stones.pop()
-        curr = [idx]
-        V[idx] = 1
-        while stones and stones[-1][0] == mn:
-            idx = stones.pop()[1]
-            curr.append(idx)
-            V[idx] = 1
-            
-        for idx in curr:
-            if idx > 0 and V[idx-1]:
-                SE[idx][0] = SE[idx-1][0]
-            if idx < n-1 and V[idx+1]:
-                SE[idx][1] = SE[idx+1][1]
-            SE[SE[idx][0]][1] = SE[idx][1]
-            SE[SE[idx][1]][0] = SE[idx][0]
-            if SE[idx][1] - SE[idx][0] + 1 >= k:
-                return mn
-        
-        cnt = mn
-    return cnt
+    for idx, val in enumerate(stones):
+
+        while q and q[-1][0] < val:
+            q.pop()
+        q.append((val, idx))
+
+        while idx - q[0][1] >= k:
+            q.popleft()
+        mxa.append(q[0][0])
+
+    return min(mxa[k-1:])
