bd58460b329f7564dbe31c1ce2b56bc4707be74f
Junseop Kim <122511728+uttamapaksa@users.noreply.github.com>
2024-07-06 10:01:26 +0900

    [Gold V] Title: 1, 2, 3 더하기 4, Time: 100 ms, Memory: 109240 KB -BaekjoonHub

--- 백준/Gold/15989. 1， 2， 3 더하기 4/1， 2， 3 더하기 4.py
+++ 백준/Gold/15989. 1， 2， 3 더하기 4/1， 2， 3 더하기 4.py
@@ -1,10 +1,8 @@
-n = int(input())
-arr = [int(input()) for _ in range(n)]
+arr = [int(input()) for _ in range(int(input()))]
 mxv = max(arr)
 
 dp = [1, 1, 2, 3, 4]
 for i in range(5, mxv+1):
     dp.append(dp[i-3] + dp[i-2] - dp[i-5] + 1)
 
-for i in arr:
-    print(dp[i])
+print('\n'.join(str(dp[i]) for i in arr))
