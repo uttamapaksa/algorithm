c810850d7be758e28d44ef3c1aae46b9e06ba043
Junseop Kim <122511728+uttamapaksa@users.noreply.github.com>
2024-07-08 14:19:19 +0900

    [Gold II] Title: 1의 개수 세기, Time: 92 ms, Memory: 108080 KB -BaekjoonHub

--- 백준/Gold/9527. 1의 개수 세기/1의 개수 세기.py
+++ 백준/Gold/9527. 1의 개수 세기/1의 개수 세기.py
@@ -1,19 +1,21 @@
-def count1(n):
-    num = bin(n)[2:]
-    N = len(num)
-
-    res = 0
-    for i in range(N-1):
-        if num[i] == '1':
-            res += dp[N-i-1]
-            res += int(num[i+1:], 2) + 1
-    res += int(num[-1])
-    return res
-
-
 A, B = map(int, input().split())
+abin, bbin = bin(A-1)[2:], bin(B)[2:]
+alen, blen = len(abin), len(bbin)
+
 dp = [0, 1]
-for i in range(2, len(bin(B)[2:])+1):
+for i in range(2, blen+1):
     dp.append(2*dp[i-1] + 2**(i-1))
 
-print(count1(B) - count1(A-1))
+ans = 0
+for i in range(blen-1):
+    if bbin[i] == '1':
+        ans += dp[blen-i-1]
+        ans += int(bbin[i+1:], 2) + 1
+ans += int(bbin[-1])
+for i in range(alen-1):
+    if abin[i] == '1':
+        ans -= dp[alen-i-1]
+        ans -= int(abin[i+1:], 2) + 1
+ans -= int(abin[-1])
+
+print(ans)
