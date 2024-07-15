0c9da4b5e41e5d7f7dcb572ffa71a682581580aa
Junseop Kim <122511728+uttamapaksa@users.noreply.github.com>
2024-07-16 07:46:25 +0900

    [Gold I] Title: 이항 계수 3, Time: 112 ms, Memory: 109240 KB -BaekjoonHub

--- 백준/Gold/11401. 이항 계수 3/이항 계수 3.py
+++ 백준/Gold/11401. 이항 계수 3/이항 계수 3.py
@@ -1,14 +1,14 @@
 P = 1000000007
 N, K = map(int, input().split())
+K = max(K, N-K)
 
-if K == 0 or K == N:
-    print(1)
-else:
-    fact = [1] * (N+1)
-    for i in range(2, N+1):
-        fact[i] = fact[i-1] * i % P
-        
-    NF = fact[N]
-    IKF = pow(fact[K], P-2, P)
-    INKF = pow(fact[N-K], P-2, P)
-    print(NF * IKF % P * INKF % P)
+res = 1
+if 0 < K < N:
+    for i in range(N, K, -1):
+        res = res * i % P
+    mod = 1
+    for i in range(1, N-K+1):
+        mod = mod * i % P
+    res = res * pow(mod, P-2, P) % P
+
+print(res)
