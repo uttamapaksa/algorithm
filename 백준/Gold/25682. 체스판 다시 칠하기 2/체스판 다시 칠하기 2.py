fb0bf51a01c3e60c16a04ec87683acc633746483
Junseop Kim <122511728+uttamapaksa@users.noreply.github.com>
2024-07-15 15:39:02 +0900

    [Gold V] Title: 체스판 다시 칠하기 2, Time: 292 ms, Memory: 142344 KB -BaekjoonHub

--- 백준/Gold/25682. 체스판 다시 칠하기 2/체스판 다시 칠하기 2.py
+++ 백준/Gold/25682. 체스판 다시 칠하기 2/체스판 다시 칠하기 2.py
@@ -1,11 +1,13 @@
+import sys; input = sys.stdin.readline
 N, M, K = map(int, input().split()); T = K*K
-dp = [[0] * (M+1)] + [[0] + [*input()] for _ in range(N)]
+dp = [[0] * (M+1) for _ in range(N+1)]
 
-for i in range(1, N+1):
-    for j in range(1, M+1):
-        if ((i+j) & 1 and dp[i][j] == 'B') or (not ((i+j) & 1) and dp[i][j] == 'W'): dp[i][j] = 1
-        else: dp[i][j] = 0
-        dp[i][j] += dp[i-1][j] + dp[i][j-1] - dp[i-1][j-1]
+for i in range(N):
+    board = input()
+    for j in range(M):
+        if ((i+j)&1 and board[j]=='B') or (not (i+j)&1 and board[j]=='W'):
+            dp[i+1][j+1] = 1
+        dp[i+1][j+1] += dp[i][j+1] + dp[i+1][j] - dp[i][j]
 
 ans = T
 for i in range(K, N+1):
