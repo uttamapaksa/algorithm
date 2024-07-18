d94e3d9caa27c88f064afa14ccb25230e422266a
Junseop Kim <122511728+uttamapaksa@users.noreply.github.com>
2024-07-19 08:08:45 +0900

    [Platinum IV] Title: 축구 전술, Time: 1528 ms, Memory: 479904 KB -BaekjoonHub

--- 백준/Platinum/3977. 축구 전술/축구 전술.py
+++ 백준/Platinum/3977. 축구 전술/축구 전술.py
@@ -1,4 +1,4 @@
-import sys; sys.setrecursionlimit(10**5)
+import sys; sys.setrecursionlimit(10**5); input=sys.stdin.readline
 
 def dfs(u):
     global id; id += 1
@@ -24,15 +24,14 @@ def dfs(u):
     return par
 
 
-tc = int(input())
-for t in range(tc):
+ans = []
+for _ in range(int(input())):
+    if ans: input(); ans.append("")
     N, M = map(int, input().split())
     G = [[] for _ in range(N)]
     for _ in range(M):
         a, b = map (int, input().split())
         G[a].append(b)
-    if t != tc-1:
-        input()
 
     id = 0
     ids = [0] * N
@@ -45,14 +44,13 @@ for t in range(tc):
         dfs(u)
 
     for u in range(N):
-        for v in G[u]:
-            if scc_ids[u] != scc_ids[v] and scc_ids[v] in scc:
-                del scc[scc_ids[v]]
+            for v in G[u]:
+                if scc_ids[u] != scc_ids[v] and scc_ids[v] in scc:
+                    del scc[scc_ids[v]]
     
     if len(scc) > 1:
-        print("Confused")
-    elif len(scc) == 1:
-        print(*sorted([*scc.values()][0]), sep='\n')
+        ans.append("Confused")
     else:
-        print(*tuple(i for i in range(N)), sep='\n')
-    print()
+        ans.append("\n".join(map(str, sorted(tuple(scc.values())[0]))))
+
+print("\n".join(ans))
