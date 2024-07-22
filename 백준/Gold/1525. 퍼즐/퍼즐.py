3ef209d477827452888d7182e35af4c319cca92a
Junseop Kim <122511728+uttamapaksa@users.noreply.github.com>
2024-07-22 20:35:00 +0900

    [Gold II] Title: 퍼즐, Time: 248 ms, Memory: 131452 KB -BaekjoonHub

--- 백준/Gold/1525. 퍼즐/퍼즐.py
+++ 백준/Gold/1525. 퍼즐/퍼즐.py
@@ -1,28 +1,27 @@
 from collections import deque
 
 
-def sol():
-    if first == last:
+def sol(s, i):
+    if s == e:
         return 0
-    queue = deque([(first, first.index('0'))])
-    visit = {first: 1}
+    queue = deque([(s, i)])
+    memo = {s: 1}
     while queue:
-        val, idx = queue.popleft()
-        cnt = visit[val]
-        val = list(val)
-        for nidx in delta[idx]:
-            val[idx], val[nidx] = val[nidx], val[idx]
-            nval = ''.join(val)
-            if nval == last:
-                return cnt
-            if nval not in visit:
-                visit[nval] = cnt + 1
-                queue.append((nval, nidx))
-            val[idx], val[nidx] = val[nidx], val[idx]
+        v, i = queue.popleft()
+        for ni in delta[i]:
+            pv = v % ten[ni+1] - v % ten[ni]
+            nv = pv // ten[ni] * ten[i]
+            nv = v - pv + nv
+            if nv == e:
+                return memo[v]
+            if nv not in memo:
+                memo[nv] = memo[v] + 1
+                queue.append((nv, ni))
     return -1
 
 
-delta = [[1,3],[0,2,4],[1,5],[0,4,6],[1,3,5,7],[2,4,8],[3,7],[4,6,8],[5,7]]
-first = ''.join(''.join(input().split()) for _ in range(3))
-last = '123456780'
-print(sol())
+delta = [[1, 3], [0, 2, 4], [1, 5], [0, 4, 6], [1, 3, 5, 7], [2, 4, 8], [3, 7], [4, 6, 8], [5, 7]]
+ten = [10 ** i for i in range(10)]
+s = ''.join(''.join(input().split()) for _ in range(3))
+e = 123456780
+print(sol(int(s), 8 - s.index('0')))
