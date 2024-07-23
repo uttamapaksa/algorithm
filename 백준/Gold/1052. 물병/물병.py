19b2835ec5959ef2e5c9f71e97456dfad041991c
Junseop Kim <122511728+uttamapaksa@users.noreply.github.com>
2024-07-23 14:14:00 +0900

    [Gold V] Title: 물병, Time: 112 ms, Memory: 108080 KB -BaekjoonHub

--- 백준/Gold/1052. 물병/물병.py
+++ 백준/Gold/1052. 물병/물병.py
@@ -1,7 +1,15 @@
 N, K = map(int, input().split())
-ans = 0
-while bin(N).count('1') > K:
-    add = N & -N
-    N += add
-    ans += add
-print(ans)
+M = [0, *map(int, bin(N)[2:])]
+if sum(M) <= K:
+    print(0)
+else:
+    cnt, idx, L = 0, 0, len(M)-1
+    for i in range(1, L):
+        if M[i]:
+            cnt += 1
+            if cnt == K:
+                break
+        else:
+            idx = i
+    M = sum(1 << L-i for i in range(idx) if M[i]) + (1 << L-idx)
+    print(M - N)
