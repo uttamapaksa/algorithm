165764a037b4f8e0ac1e3704475003f56370345b
Junseop Kim <uttamapaksa@gmail.com>
2024-07-30 10:12:38 +0900

    [Silver III] Title: 과제는 끝나지 않아!, Time: 640 ms, Memory: 181536 KB -BaekjoonHub

--- 백준/Silver/17952. 과제는 끝나지 않아！/과제는 끝나지 않아！.py
+++ 백준/Silver/17952. 과제는 끝나지 않아！/과제는 끝나지 않아！.py
@@ -1,15 +1,7 @@
-ans = 0
-stack = []  # score, time
-for _ in range(int(input())):
-    ipt = tuple(map(int, input().split()))
-    if not ipt[0]:
-        if stack:
-            if stack[-1][1] == 1:
-                ans += stack.pop()[0]
-                continue
-            stack[-1][1] -= 1
-    elif ipt[2] == 1:
-        ans += ipt[1]
-    else:
-        stack.append([ipt[1], ipt[2]-1])
-print(ans)
+a=0;s=[[0,-1]];t=int;i=input
+for _ in range(t(i())):
+    p=i().split()
+    if p[0]=='0':s[-1][1]-=1
+    else:s+=[[t(p[1]),t(p[2])-1]]
+    if s[-1][1]==0:a+=s.pop()[0]
+print(a)
