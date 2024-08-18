f1f63cbefd18eb33283593dfcd9141c68b50aa1d
Junseop Kim <uttamapaksa@gmail.com>
2024-08-18 10:36:25 +0900

    [Silver III] Title: 대표 자연수, Time: 164 ms, Memory: 11120 KB -BaekjoonHub

--- 백준/Silver/2548. 대표 자연수/대표 자연수.ts
+++ 백준/Silver/2548. 대표 자연수/대표 자연수.ts
@@ -1,24 +1,17 @@
 // input
 const [[N], iarr]: number[][] = require('fs').readFileSync(process.platform==='linux'?0:'input.txt').toString().trim().split('\n').map((x:string)=>x.trim().split(' ').map(x=>+x));
-const arr: number[] = iarr.sort((a,b) => a-b);
 // init
-let diff = arr.reduce((a,c) => a+c-arr[0], 0);
+const arr: number[] = iarr.sort((a,b) => a-b);
+let total = arr.reduce((a,c) => a+c, 0) - N*arr[0];
 let ans = arr[0];
 // calc
 for (let i=1; i<N; i++) {
-  const v = arr[i];
-  if (arr[i-1] === v) continue;
-  let tmpDiff = 0;
-  for (let j=0; j<i; j++) {
-    tmpDiff += v - arr[j]
-  }
-  for (let j=i+1; j<N; j++) {
-    tmpDiff += arr[j] - v
-  }
-  if (diff > tmpDiff) {
-    diff = tmpDiff;
-    ans = v;
-  };
+  const diff = arr[i] - arr[i-1]
+  if (diff === 0) continue;
+  const delta = i*diff - (N-i)*diff 
+  total += delta;
+  if (delta >= 0) continue;
+  ans = arr[i];
 }
 // output
 console.log(ans);
