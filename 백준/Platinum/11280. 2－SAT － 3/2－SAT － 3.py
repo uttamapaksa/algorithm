daba1ef25353a9f296b1e0e6bdec2118964ad778
Junseop Kim <uttamapaksa@gmail.com>
2024-08-31 22:22:12 +0900

    [Platinum IV] Title: 2-SAT - 3, Time: 752 ms, Memory: 492512 KB -BaekjoonHub

--- 백준/Platinum/11280. 2－SAT － 3/2－SAT － 3.py
+++ 백준/Platinum/11280. 2－SAT － 3/2－SAT － 3.py
@@ -1,4 +1,4 @@
-import sys; sys.setrecursionlimit(10**4)
+import sys; sys.setrecursionlimit(10**4); input=sys.stdin.readline
 
 N, M = map(int, input().split())
 L = 2*(N+1)
@@ -17,15 +17,18 @@ def tarjan(u):
     for v in G[u]:
         if not ids[v]:
             par = min(par, tarjan(v))
+            if not ans:
+                return 0
         elif not finished[v]:
             par = min(par, ids[v])
     if par == ids[u]:
         curr = set()
         while 1:
             v = stack.pop()
-            finished[v] = True
             if -v in curr:
                 ans = 0
+                return 0
+            finished[v] = True
             curr.add(v)
             if u == v:
                 break
@@ -36,7 +39,9 @@ ids = [0] * L
 finished = [False] * L
 stack = []
 ans = 1
-for u in range(L):
+for u in range(1, L):
+    if not ans:
+        break
     if not ids[u]:
         tarjan(u)
 
