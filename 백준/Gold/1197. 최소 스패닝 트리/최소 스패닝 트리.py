f1297a0ddded972fbc5547c3553a5dae3558a1a1
Junseop Kim <uttamapaksa@gmail.com>
2024-09-10 06:23:21 +0900

    [Gold IV] Title: 최소 스패닝 트리, Time: 284 ms, Memory: 48676 KB -BaekjoonHub

--- 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.py
+++ 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.py
@@ -1,30 +1,25 @@
-def find(x):
-    while x != P[x]:
-        x = P[x]
-    return x
+import sys; sys.setrecursionlimit(10001); input=sys.stdin.readline
 
-def union(x, y):
-    P[find(y)] = find(x)
+def find(x):
+    if x != P[x]:
+        P[x] = find(P[x])
+    return P[x]
 
 V, E = map(int, input().split())
 P = [i for i in range(V+1)]
 G = []
-MST = []
-
 for _ in range(E):
     s, e, w = map(int, input().split())
     G.append((w, s, e))
 G.sort()
 
-cnt = 0
-for w, s, e in G:
-    if find(s) != find(e):
-        cnt += 1
-        MST.append((s, e, w))
-        union(s, e)
-        if cnt == V: break
-
 ans = 0
-for s, e, w in MST:
-    ans += w
+for w, s, e in G:
+    a, b = find(s), find(e)
+    if a != b: 
+        ans += w
+        if a > b:
+            a, b = b, a
+        P[b] = a
+
 print(ans)
