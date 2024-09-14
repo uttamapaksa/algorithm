4b09738c9f5462d2639855c1b99149713fd1410d
Junseop Kim <uttamapaksa@gmail.com>
2024-09-15 08:43:51 +0900

    [Platinum III] Title: 디지털 비디오 디스크(DVDs), Time: 2056 ms, Memory: 248592 KB -BaekjoonHub

--- 백준/Platinum/9345. 디지털 비디오 디스크（DVDs）/디지털 비디오 디스크（DVDs）.py
+++ 백준/Platinum/9345. 디지털 비디오 디스크（DVDs）/디지털 비디오 디스크（DVDs）.py
@@ -1,45 +1,62 @@
 import sys; input=sys.stdin.readline
 
+
+def update():
+    tree[size+A], tree[size+B] = tree[size+B], tree[size+A]
+    i = size+A
+    while i:
+        i >>= 1 
+        left = tree[i*2]
+        right = tree[i*2+1]
+        tree[i] = [min(left[0], right[0]), max(left[1], right[1])]
+    i = size+B
+    while i:
+        i >>= 1 
+        left = tree[i*2]
+        right = tree[i*2+1]
+        tree[i] = [min(left[0], right[0]), max(left[1], right[1])]
+
+
+def query():
+    minv, maxv = B+1, A-1
+    l, r = size+A, size+B
+    while l <= r:
+        if l & 1:
+            minv = min(minv, tree[l][0])
+            maxv = max(maxv, tree[l][1])
+            l += 1
+        if not r & 1:
+            minv = min(minv, tree[r][0])
+            maxv = max(maxv, tree[r][1])
+            r -= 1
+        l >>= 1; r >>= 1
+    ans.append('YES' if minv == A and maxv == B else 'NO')
+
+
 ans = []
 for _ in range(int(input())):
     # input
     N, K = map(int, input().split())
+
+    # init
     size = 1
     while size < N:
         size <<= 1
     tree = [[0, 0] for _ in range(size*2)]  # min, max
-    # init
     for i in range(N):
         tree[size+i] = [i, i]
-    for i in range(size*2-2, 0, -2):
-        tree[i>>1] = [min(tree[i][0], tree[i+1][0]), max(tree[i][1], tree[i+1][1])]
+    for i in range(size-1, 0, -1):
+        left = tree[i*2]
+        right = tree[i*2+1]
+        tree[i] = [min(left[0], right[0]), max(left[1], right[1])]
+
+    # operation
     for _ in range(K):
         Q, A, B = map(int, input().split())
-        # query
         if Q:
-            l, r = size+A, size+B
-            minv, maxv = B+1, A-1
-            while l <= r:
-                if l & 1:
-                    minv = min(minv, tree[l][0])
-                    maxv = max(maxv, tree[l][1])
-                    l += 1
-                if not r & 1:
-                    minv = min(minv, tree[r][0])
-                    maxv = max(maxv, tree[r][1])
-                    r -= 1
-                l >>= 1; r >>= 1
-            ans.append('YES' if minv == A and maxv == B else 'NO')
-        # update
+            query()
         else:
-            tree[size+A], tree[size+B] = tree[size+B], tree[size+A]
-            i = (size+A) >> 1
-            while i:
-                tree[i] = [min(tree[i*2][0], tree[i*2+1][0]), max(tree[i*2][1], tree[i*2+1][1])]
-                i >>= 1
-            i = (size+B) >> 1
-            while i:
-                tree[i] = [min(tree[i*2][0], tree[i*2+1][0]), max(tree[i*2][1], tree[i*2+1][1])]
-                i >>= 1
+            update()
+
 # output
 print('\n'.join(ans))
