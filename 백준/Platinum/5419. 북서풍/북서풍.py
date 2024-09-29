a7d0a2033ae947061a39a489031e95f659a91f0d
Junseop Kim <uttamapaksa@gmail.com>
2024-09-29 11:42:11 +0900

    [Platinum III] Title: 북서풍, Time: 2080 ms, Memory: 215872 KB -BaekjoonHub

--- 백준/Platinum/5419. 북서풍/북서풍.py
+++ 백준/Platinum/5419. 북서풍/북서풍.py
@@ -1,35 +1,30 @@
 for _ in range(int(input())):
     n = int(input())
-    # x가 작은 순, y가 큰 순으로 정렬 
-    arr = sorted([[*map(int, input().split()), i] for i in range(n)], key=lambda x: (x[0], -x[1]))
-    # 세그먼트 트리에 사용될 y 좌표
-    ys = sorted(set(v[1] for v in arr))
-    # 특정 y 좌표가 세그먼트 트리에서 몇번째 노드인지 매핑 
+    # x가 큰 순, y가 작은 순으로 정렬
+    arr = sorted([tuple(map(int, input().split())) for _ in range(n)], key=lambda x: (-x[0], x[1]))
+    # 좌표 압축. 펜윅 트리에 사용될 y 좌표
+    ys = sorted(set(v[1] for v in arr)) 
+    # 특정 y 좌표가 펜윅 트리에서 몇번째 노드인지 매핑. sum(0) - sum(-1)을 방지하기 위해 i + 1
     m = len(ys)
-    ysi = {ys[i]: i for i in range(m)} 
-    # 세그먼트 트리 생성
-    size = 1
-    while size < m:
-        size <<= 1
-    tree = [0] * (2 * size)
+    ysi = {ys[i]: i + 1 for i in range(m)} 
+    # 펜윅 트리 생성
+    tree = [0] * (m + 1)
     # 스위핑
     ans = 0
-    for x, y, i in arr:
+    for x, y in arr:
         # 구간 쿼리
-        l = size + ysi[y]
-        r = 2 * size -1
-        while l <= r:
-            if l & 1:
-                ans += tree[l]
-                l += 1
-            if not r & 1:
-                ans += tree[r]
-                r -= 1
-            l >>= 1; r >>= 1
+        # 이 문제는 y가 더 높은 점들이 이전 점들인데, 펜윅 트리는 무조건 트리의 0번째 노드부터 더하기 때문에 이후 점들을 먼저 계산
+        # 그래서 거꾸로 이후 점에서 이전 점으로 누적합으로 구하기 위해서 arr를 y가 작은(이후 점) 순으로 정렬해야 함
+        idx = ysi[y]
+        while idx > 0:
+            ans += tree[idx]
+            idx -= idx & -idx
+        idx = ysi[y]
+        cnt = 1
         # 구간 업데이트
-        l = size + ysi[y]
-        while l:
-            tree[l] += 1
-            l >>= 1
+        while idx <= m:
+            cnt += 1
+            tree[idx] += 1
+            idx += idx & -idx
     # 출력
     print(ans)
