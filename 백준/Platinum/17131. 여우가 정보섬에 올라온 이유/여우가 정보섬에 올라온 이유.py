085a9e5e2f1a4a3057df73e23ba4c427106439b8
Junseop Kim <uttamapaksa@gmail.com>
2024-10-03 17:06:48 +0900

    [Platinum IV] Title: 여우가 정보섬에 올라온 이유, Time: 812 ms, Memory: 205148 KB -BaekjoonHub

--- 백준/Platinum/17131. 여우가 정보섬에 올라온 이유/여우가 정보섬에 올라온 이유.py
+++ 백준/Platinum/17131. 여우가 정보섬에 올라온 이유/여우가 정보섬에 올라온 이유.py
@@ -1,69 +1,64 @@
 MOD = 1000000007
 N = int(input())
-arr = [(*map(int, input().split()), i) for i in range(N)]
-coo = {v: i for i, v in enumerate(sorted({v[1] for v in arr}))}
+arr = [tuple(map(int, input().split())) for _ in range(N)]
+arr.sort(key=lambda x: x[0])
+coo = {v: i+1 for i, v in enumerate(sorted({v[1] for v in arr}))}
 M = len(coo)
-size = 1
-while size < M:
-    size <<= 1
-ans = 0
 
-arr.sort(key=lambda x: (x[0], -x[1]))
-tree = [0] * (2 * size)
-count = [0] * N
-sames, same = [], 200000
-for x, y, i in arr:
+tree = [0] * (M+1)
+sames, same = [], 100001
+count1 = [0] * N
+for i in range(N):
+    x, y = arr[i]
     if same == x:
         sames.append((y, i))
     else:
+        total = 0
+        idx = M
+        while idx:
+            total += tree[idx]
+            idx -= idx & -idx
         for py, pi in sames:
-            if coo[py] == M-1: continue
-            l = size + coo[py]+1
-            r = 2 * size - 1
-            while l <= r:
-                if l & 1:
-                    count[pi] += tree[l]
-                    l += 1
-                if not r & 1:
-                    count[pi] += tree[r]
-                    r -= 1
-                l >>= 1; r >>= 1
-            count[pi] %= MOD
+            curr = total
+            idx = coo[py]
+            while idx:
+                curr -= tree[idx]
+                idx -= idx & -idx
+            count1[pi] = curr % MOD
         for py, pi in sames:
-            l = size + coo[py]
-            while l:
-                tree[l] += 1
-                l >>= 1
+            idx = coo[py]
+            while idx <= M:
+                tree[idx] += 1
+                idx += idx & -idx
         same = x
         sames = [(y, i)]
 
-arr.sort(key=lambda x: (-x[0], -x[1]))
-tree = [0] * (2 * size)
-sames, same = [], 200000
-for x, y, i in arr:
+tree = [0] * (M+1)
+sames, same = [], 100001
+ans = 0
+for i in range(N-1, -1, -1):
+    x, y = arr[i]
     if same == x:
         sames.append((y, i))
     else:
+        total = 0
+        idx = M
+        while idx:
+            total += tree[idx]
+            idx -= idx & -idx
         for py, pi in sames:
-            pv = 0
-            if coo[py] == M-1: continue
-            l = size + coo[py]+1
-            r = 2 * size - 1
-            while l <= r:
-                if l & 1:
-                    pv += tree[l]
-                    l += 1
-                if not r & 1:
-                    pv += tree[r]
-                    r -= 1
-                l >>= 1; r >>= 1
-            pv %= MOD
-            ans = (ans + (pv * count[pi]) % MOD) % MOD
+            curr = total
+            idx = coo[py]
+            while idx:
+                curr -= tree[idx]
+                idx -= idx & -idx
+            ans += ((curr % MOD) * count1[pi]) % MOD
+            ans %= MOD
         for py, pi in sames:
-            l = size + coo[py]
-            while l:
-                tree[l] += 1
-                l >>= 1
+            idx = coo[py]
+            while idx <= M:
+                tree[idx] += 1
+                idx += idx & -idx
         same = x
         sames = [(y, i)]
 
