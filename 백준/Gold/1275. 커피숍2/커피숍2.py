a0049895cebd5cdc34468861cde40197a0286eda
Junseop Kim <uttamapaksa@gmail.com>
2024-10-09 19:29:51 +0900

    [Gold I] Title: 커피숍2, Time: 204 ms, Memory: 128216 KB -BaekjoonHub

--- 백준/Gold/1275. 커피숍2/커피숍2.py
+++ 백준/Gold/1275. 커피숍2/커피숍2.py
@@ -1,28 +1,36 @@
-def query(idx):
-    res = 0
-    while idx:
-        res += tree[idx] 
-        idx -= idx & -idx
-    return res
-
-def update(idx, val):
-    while idx <= N:
-        tree[idx] += val
-        idx += idx & -idx
-
+import sys; input=sys.stdin.readline
+# input
 N, Q = map(int, input().split())
 arr = [*map(int, input().split())]
 tree = [0] * (N+1)
+# build
 for i in range(N):
-    update(i+1, arr[i])
+    idx = i+1
+    while idx <= N:
+        tree[idx] += arr[i]
+        idx += idx & -idx
 
 ans = []
 for _ in range(Q):
     x, y, a, b = map(int, input().split())
-    if x > y: x, y = y, x
-    ans.append(query(y) - query(x-1))
-    delta = b - arr[a-1]
-    arr[a-1] += delta
-    update(a, delta)
-
+    if x > y:
+        x, y = y, x
+    # query y
+    ry = 0
+    while y:
+        ry += tree[y] 
+        y -= y & -y
+    # query x
+    x -= 1
+    rx = 0
+    while x:
+        rx += tree[x] 
+        x -= x & -x
+    ans.append(ry - rx)
+    # update
+    arr[a-1], delta = b, b - arr[a-1]
+    while a <= N:
+        tree[a] += delta
+        a += a & -a
+# output
 print('\n'.join(map(str, ans)))
