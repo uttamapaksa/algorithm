faa051420a5187b6fdf83717432cf587bfdb4989
Junseop Kim <uttamapaksa@gmail.com>
2024-11-04 09:26:01 +0900

    [Silver II] Title: 차이를 최대로, Time: 164 ms, Memory: 10164 KB -BaekjoonHub

--- 백준/Silver/10819. 차이를 최대로/차이를 최대로.js
+++ 백준/Silver/10819. 차이를 최대로/차이를 최대로.js
@@ -1,29 +1,27 @@
-function comb(k) {
+function comb(k, s, p) {
   if (k == N) {
-    tmpAns = 0;
-    for (let i = 0; i < N - 1; i++) {
-      tmpAns += Math.abs(tmpArr[i + 1] - tmpArr[i]);
+    if (ans < s) {
+      ans = s;
     }
-    ans = Math.max(ans, tmpAns);
     return;
   }
   for (let i = 0; i < N; i++) {
     if (visit[i]) continue;
     visit[i] = 1;
-    tmpArr[k] = arr[i];
-    comb(k + 1);
-    tmpArr[k] = arr[i];
+    comb(k + 1, s + Math.abs(p - arr[i]), arr[i]);
     visit[i] = 0;
   }
 }
 
-const input = require('fs').readFileSync(0).toString().trim().split('\n');
+const input = require('fs').readFileSync(process.platform === 'linux' ? 0 : 'input.txt').toString().trim().split('\n');
 const N = +input[0];
 const arr = input[1].split(' ').map(Number);
-const tmpArr = Array(N).fill(0);
 const visit = Array(N).fill(0);
-let ans = 0;
-let tmpAns = 0;
 
-comb(0);
+let ans = 0;
+for (let i=0; i<N; i++) {
+  visit[i] = 1
+  comb(1, 0, arr[i]);
+  visit[i] = 0
+}
 console.log(ans);
