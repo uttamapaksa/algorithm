521dc2fb0ffd1c08d2e8204659893b425c2f0c9a
Junseop Kim <uttamapaksa@gmail.com>
2024-11-09 11:42:46 +0900

    [Gold II] Title: 반도체 설계, Time: 184 ms, Memory: 16220 KB -BaekjoonHub

--- 백준/Gold/2352. 반도체 설계/반도체 설계.js
+++ 백준/Gold/2352. 반도체 설계/반도체 설계.js
@@ -1,12 +1,12 @@
 let[_,a]=require('fs').readFileSync(0).toString().split('\n').map(x=>x.split(' ').map(x=>+x));r=[];s=e=m=0
-a.map(v=>{
+for (let v of a){
   s=0;e=r.length
   while(s<e){
-    m=parseInt((s+e)/2)
+    m=(s+e)>>1
     if(v>r[m])s=m+1
     else e=m
   }
   if(s==r.length)r.push(v)
-  else r[s]=v 
-})
+    else r[s]=v 
+}
 console.log(r.length)
