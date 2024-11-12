3e68c7e8ba5c3546e0aa30c5730b066d900a9222
Junseop Kim <uttamapaksa@gmail.com>
2024-11-13 08:38:44 +0900

    [Gold III] Title: 과제, Time: 100 ms, Memory: 108080 KB -BaekjoonHub

--- 백준/Gold/13904. 과제/과제.py
+++ 백준/Gold/13904. 과제/과제.py
@@ -1,14 +1,17 @@
+import sys; input=sys.stdin.readline
 from heapq import heappop, heappush
 
 N = int(input())
-arr = [tuple(map(int, input().split())) for _ in range(N)]
-arr.sort()
+arr = [[] for _ in range(1001)]
+for _ in range(N):
+    d, w = map(int, input().split())
+    arr[d].append(-w)
 
 ans = 0
 heap = []
-for day in range(N, 0, -1):
-    while arr and arr[-1][0] >= day:
-        heappush(heap, -arr.pop()[1])
+for day in range(1000, 0, -1):
+    for w in arr[day]:
+        heappush(heap, w)
     if heap:
         ans -= heappop(heap)
 
