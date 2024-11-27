f3faebaa713416d67ab12a5860fd784f1fe5b416
Junseop Kim <uttamapaksa@gmail.com>
2024-11-27 23:58:32 +0900

    [Gold V] Title: 알약, Time: 92 ms, Memory: 9672 KB -BaekjoonHub

--- 백준/Gold/4811. 알약/알약.js
+++ 백준/Gold/4811. 알약/알약.js
@@ -1,9 +1,10 @@
-const arr = require('fs').readFileSync(process.platform === 'linux' ? 0 : 'input.txt').toString().trim().split('\n').map(Number);
+const arr = require('fs').readFileSync(0).toString().trim().split('\n').map(Number);
 const dp = [1];
 for (let i=0; i<30; i++) {
     dp.push(dp.reduce((a,c,i) => a+c*dp[dp.length-1-i], 0));
 }
-arr.pop();
-arr.forEach(v => {
-    console.log(dp[v]);
-})
+const ans = [];
+for (let i=0; i<arr.length-1; i++) {
+    ans.push(dp[arr[i]]);
+}
+console.log(ans.join('\n'));
