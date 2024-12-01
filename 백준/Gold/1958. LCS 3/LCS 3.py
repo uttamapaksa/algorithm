fd0a0467b88156740edde03f51f3c4a79b25f4df
Junseop Kim <uttamapaksa@gmail.com>
2024-12-01 23:04:46 +0900

    [Gold IV] Title: LCS 3, Time: 148 ms, Memory: 120048 KB -BaekjoonHub

---
+++ 백준/Gold/1958. LCS 3/LCS 3.py
@@ -0,0 +1,12 @@
+a = input(); b = input(); c = input()
+n, m, l = len(a), len(b), len(c)
+dp = [[[0] * (l+1) for _ in range(m+1)] for _ in range(n+1)]
+for i in range(1, n+1):
+    for j in range(1, m+1):
+        for k in range(1, l+1):
+            if a[i-1] == b[j-1] == c[k-1]:
+                dp[i][j][k] = dp[i-1][j-1][k-1] + 1
+            else:
+                dp[i][j][k] = max(dp[i-1][j][k], dp[i][j-1][k], dp[i][j][k-1])
+
+print(dp[n][m][l])
