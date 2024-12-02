60757eadf5747ccaf35c36d37e4478374eb4788d
Junseop Kim <uttamapaksa@gmail.com>
2024-12-03 02:23:19 +0900

    [Gold III] Title: 옷걸이걸이걸이, Time: 2324 ms, Memory: 501560 KB -BaekjoonHub

--- 백준/Gold/23816. 옷걸이걸이걸이/옷걸이걸이걸이.py
+++ 백준/Gold/23816. 옷걸이걸이걸이/옷걸이걸이걸이.py
@@ -7,13 +7,12 @@ else:
     if m >= 3: dp[1][3] = 2
     if m >= 7: dp[1][7] = 4
     if m >= 15:  dp[1][15] = 0
-    
+    for i in range(1, n+1):
+        for j in range(1, i+1):
+            dp[i][j] = j
     for i in range(2, n+1):
-        for j in range(m, 0, -1):
-            if i >= j:
-                dp[i][j] = j
-            else:
-                for w, v in ((1,1),(3,2),(7,4),(15,0)):
-                    if j >= w and dp[i-1][j-w] != -1 and dp[i][j] < dp[i-1][j-w] + v:
-                        dp[i][j] = dp[i-1][j-w] + v
+        for j in range(i+1, m+1):
+            for w, v in ((3,2),(7,4),(15,0)):
+                if j >= w and dp[i-1][j-w] != -1 and dp[i][j] < dp[i-1][j-w] + v:
+                    dp[i][j] = dp[i-1][j-w] + v
     print(dp[n][m])
