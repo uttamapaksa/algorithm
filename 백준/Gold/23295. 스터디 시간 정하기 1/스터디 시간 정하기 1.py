5fa5d19de39dcb99acd5316cd2b7c80fe9421f78
Junseop Kim <uttamapaksa@gmail.com>
2024-12-07 15:03:48 +0900

    [Gold III] Title: 스터디 시간 정하기 1, Time: 184 ms, Memory: 113708 KB -BaekjoonHub

--- 백준/Gold/23295. 스터디 시간 정하기 1/스터디 시간 정하기 1.py
+++ 백준/Gold/23295. 스터디 시간 정하기 1/스터디 시간 정하기 1.py
@@ -1,21 +1,22 @@
-M = 100000
+import sys;input=sys.stdin.readline
+
 N, T = map(int, input().split())
-tree = [0] * (M+1)
+tree = [0] * 100001
 for _ in range(N):
     for _ in range(int(input())):
         s, e = map(int, input().split())
         tree[e] += 1
         tree[s] -= 1
-for i in range(M-1, -1, -1):
+for i in range(99999, -1, -1):
     tree[i] += tree[i+1]
 
-cur = sum(tree[:T+1]) 
+cur = sum(tree[i] for i in range(T+1))
 mxv = cur
 idx = 0
-for i in range(T+1, M+1):
+for i in range(T+1, 100001):
     cur += tree[i] - tree[i-T]
     if mxv < cur:
-        mxv = cur
+        mxv = cur                                                                                                                                                                                                                                                                                                                                                                                                           
         idx = i-T
 
 print(idx, idx+T)
